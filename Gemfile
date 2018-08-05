a700a4b9f6c88729065e8351b18657916f6e3ce4
Renan <renanthompsom@gmail.com>
2018-08-05 19:27:22 -0300

    Setup rails i18n gem

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem 'bootstrap', '~> 4.0.0'
 gem 'jquery-rails'
 gem 'bootsnap', '>= 1.1.0', require: false
 gem 'simple_form'
+gem 'rails-i18n'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,10 @@ module SistemaGestaoTcc
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.time_zone = 'Brasilia'
+ 
+    config.i18n.enforce_available_locales = false
+    config.i18n.available_locales = ["pt-BR"]
+    config.i18n.default_locale = :'pt-BR'
   end
 end
