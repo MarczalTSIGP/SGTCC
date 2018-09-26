9f6ac28f707131101fafe3a2740ecaf7e513dac5
Renan <renanthompsom@gmail.com>
2018-09-26 20:26:35 -0300

    Add validation on email and fix header responsive

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -2,4 +2,10 @@ class Professor < ApplicationRecord
   devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
 
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+  validates :email, presence: true,
+      length: { maximum: 255 },
+      format:     { with: VALID_EMAIL_REGEX },
+      uniqueness: { case_sensitive: false }
 end
