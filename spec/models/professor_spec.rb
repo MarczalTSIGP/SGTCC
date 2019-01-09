67b6b14c62b784cb5b8b12a3e550ffa5e76bec7a
Renan <renanthompsom@gmail.com>
2019-01-09 16:30:11 -0200

    Fix professor unit tests

--- spec/models/professor_spec.rb
+++ spec/models/professor_spec.rb
@@ -1,26 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Professor, type: :model do
-  let(:professor) { create(:professor) }
+  describe 'validates' do
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_length_of(:email) }
+    it { is_expected.to validate_length_of(:password) }
 
-  it 'is valid with valid attributes' do
-    expect(professor).to be_valid
-  end
-
-  it 'is not valid with email too long' do
-    professor.email = 'a' * 256 + '@example.com'
-    expect(professor).not_to be_valid
-  end
-
-  it 'is not valid with email unique' do
-    duplicate_professor = professor.dup
-    duplicate_professor.email = professor.email.upcase
-    professor.save
-    expect(duplicate_professor).not_to be_valid
-  end
-
-  it 'is not valid with password minimum length' do
-    professor.password = professor.password_confirmation = 'a' * 5
-    expect(professor).not_to be_valid
+    context 'with email' do
+      it { is_expected.to allow_value('email@addresse.foo').for(:email) }
+      it { is_expected.not_to allow_value('foo').for(:email) }
+    end
   end
 end
