877413f737ec2cdc4a148386a69adb9b26ddd98a
Renan <renanthompsom@gmail.com>
2019-02-22 08:46:59 -0300

    Remove unused files

--- app/javascript/utils/i18n/messages.js
+++ app/javascript/utils/i18n/messages.js
@@ -4,20 +4,7 @@ export const messages = {
       registration: {
         edit_image: 'Clique na imagem para alterá-la'
       },
-      confirm: {
-        delete: 'Você tem certeza que deseja deletar?'
-      },
-      loading: 'Carregando...',
-      links: {
-        show: 'Visualizar',
-        edit: 'Editar',
-        delete: 'Deletar'
-      },
-      pagination: {
-        next: 'Próximo',
-        previous: 'Anterior'
-      },
-    }
+    },
   },
   'en': {
     messages: {
