27d03acf750b14e24edd1f0b9d5ad60c20cee062
Renan <renanthompsom@gmail.com>
2019-02-26 16:01:25 -0300

    Remove unused method

--- lib/devise/custom_failure.rb
+++ lib/devise/custom_failure.rb
@@ -2,13 +2,4 @@ class CustomFailure < Devise::FailureApp
   def redirect_url
     new_responsible_session_url
   end
-
-  # You need to override respond to eliminate recall
-  def respond
-    if http_auth?
-      http_auth
-    else
-      redirect
-    end
-  end
 end
