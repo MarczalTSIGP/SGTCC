a930e85c9d36a3d66c17c6570fb2ec9364728bad
Renan <renanthompsom@gmail.com>
2019-02-26 15:49:30 -0300

    Remove redirect from home controller test

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe HomeController, type: :controller do
   describe 'GET #index' do
     it 'returns http success' do
       get :index
-      expect(response).to redirect_to(new_responsible_session_path)
+      expect(response).to have_http_status(:success)
     end
   end
 end
