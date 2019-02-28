d74881d78d4d54bfd686a3dc6cbd2586cababe0d
Renan <renanthompsom@gmail.com>
2019-02-28 17:26:08 -0300

    Add model tests for lattes and gender attribute

--- spec/models/professor_spec.rb
+++ spec/models/professor_spec.rb
@@ -5,7 +5,11 @@ RSpec.describe Professor, type: :model do
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_presence_of(:username) }
     it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_presence_of(:lattes) }
+    it { is_expected.to validate_presence_of(:gender) }
+
     it { is_expected.to validate_presence_of(:password) }
+
     it { is_expected.to validate_length_of(:email) }
     it { is_expected.to validate_length_of(:password) }
     it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
@@ -17,6 +21,14 @@ RSpec.describe Professor, type: :model do
 
     context 'when email is not valid' do
       it { is_expected.not_to allow_value('foo').for(:email) }
+    end
+
+    context 'when lattes is valid' do
+      it { is_expected.to allow_value('http://lattes.com/link').for(:lattes) }
+    end
+
+    context 'when lattes is not valid' do
+      it { is_expected.not_to allow_value('lattes.com').for(:lattes) }
     end
   end
 end
