17c68ec638e7bf533ac35ab298984727442b6127
Renan <renanthompsom@gmail.com>
2019-02-28 17:19:42 -0300

    Add model validations

--- app/models/academic.rb
+++ app/models/academic.rb
@@ -7,11 +7,16 @@ class Academic < ApplicationRecord
 
   enum gender: { male: 'M', female: 'F' }, _prefix: :gender
 
-  validates :name,   presence: true
-  validates :gender, presence: true
+  validates :name,
+            presence: true
+
+  validates :gender,
+            presence: true
+
   validates :ra,
             presence: true,
             uniqueness: true
+
   validates :email,
             presence: true,
             format: { with: Devise.email_regexp },
--- app/models/professor.rb
+++ app/models/professor.rb
@@ -3,13 +3,27 @@ class Professor < ApplicationRecord
          :rememberable, :validatable,
          authentication_keys: [:username]
 
-  validates :name, presence: true
-  validates :username, presence: true,
-                       uniqueness: { case_sensitive: false }
-  validates :email, presence: true,
-                    length: { maximum: 255 },
-                    format: { with: Devise.email_regexp },
-                    uniqueness: { case_sensitive: false }
+  enum gender: { male: 'M', female: 'F' }, _prefix: :gender
+
+  validates :name,
+            presence: true
+
+  validates :gender,
+            presence: true
+
+  validates :username,
+            presence: true,
+            uniqueness: { case_sensitive: false }
+
+  validates :lattes,
+            presence: true,
+            format: { with: URI::DEFAULT_PARSER.make_regexp }
+
+  validates :email,
+            presence: true,
+            length: { maximum: 255 },
+            format: { with: Devise.email_regexp },
+            uniqueness: { case_sensitive: false }
 
   mount_uploader :profile_image, ProfileImageUploader
 
