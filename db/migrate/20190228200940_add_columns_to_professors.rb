9c5c267c9cf94db3c5d0ea9cbc3549b769b8bde6
Renan <renanthompsom@gmail.com>
2019-02-28 17:14:24 -0300

    Add lattes, gender, is_active and available advisor to professors table

---
+++ db/migrate/20190228200940_add_columns_to_professors.rb
@@ -0,0 +1,8 @@
+class AddColumnsToProfessors < ActiveRecord::Migration[5.2]
+  change_table :professors, bulk: true do |t|
+    t.string :lattes
+    t.string :gender, limit: 1
+    t.boolean :is_active, default: false
+    t.boolean :available_advisor, defalt: false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_27_113434) do
+ActiveRecord::Schema.define(version: 2019_02_28_200940) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,10 @@ ActiveRecord::Schema.define(version: 2019_02_27_113434) do
     t.string "profile_image"
     t.string "username"
     t.string "name"
+    t.string "lattes"
+    t.string "gender", limit: 1
+    t.boolean "is_active", default: false
+    t.boolean "available_advisor"
     t.index ["email"], name: "index_professors_on_email", unique: true
     t.index ["reset_password_token"], name: "index_professors_on_reset_password_token", unique: true
     t.index ["username"], name: "index_professors_on_username", unique: true
