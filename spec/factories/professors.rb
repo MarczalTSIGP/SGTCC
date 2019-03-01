110f21ecfb48998c30cb03641278485401b363f2
Renan <renanthompsom@gmail.com>
2019-03-01 14:51:55 -0300

    Add new fields to professor factory and fix responsible profile test

--- spec/factories/professors.rb
+++ spec/factories/professors.rb
@@ -3,6 +3,10 @@ FactoryBot.define do
     sequence(:name) { |n| "Professor #{n}" }
     sequence(:username) { |n| "professor#{n}" }
     sequence(:email) { |n| "professor#{n}@gmail.com" }
+    sequence(:lattes) { |n| "http://lattes.com.#{n}" }
+    gender { Professor.genders.values.sample }
+    is_active { Faker::Boolean.boolean }
+    available_advisor { Faker::Boolean.boolean }
     password { 'password' }
     password_confirmation { 'password' }
   end
