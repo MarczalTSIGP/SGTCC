597443cadb2480082649a869508333cb4d4cfabb
renangabriel27 <renanthompsom@gmail.com>
2019-03-21 23:25:59 -0300

    Refactor method search

--- app/models/concerns/searchable.rb
+++ app/models/concerns/searchable.rb
@@ -5,15 +5,13 @@ module Searchable
 
   included do
     def self.search(term = nil, search_fields = [], order_by = 'name ASC')
-      if term
-        search = search_fields.map do |name, value|
-          value[:unaccent] ? "unaccent(#{name}) ILIKE unaccent(?)" : "#{name} ILIKE ?"
-        end.join(' OR ')
+      return order(order_by) if term.blank?
 
-        where(search, *["%#{term}%"] * search_fields.count).order(order_by)
-      else
-        order(order_by)
-      end
+      search = search_fields.map do |name, value|
+        value[:unaccent] ? "unaccent(#{name}) ILIKE unaccent(?)" : "#{name} ILIKE ?"
+      end.join(' OR ')
+
+      where(search, *["%#{term}%"] * search_fields.count).order(order_by)
     end
   end
 end
