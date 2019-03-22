755d7221a357cd97908f1abf5a84c765fc83556e
renangabriel27 <renanthompsom@gmail.com>
2019-03-21 23:33:27 -0300

    Fix rubocop offense

--- app/models/concerns/searchable.rb
+++ app/models/concerns/searchable.rb
@@ -7,11 +7,15 @@ module Searchable
     def self.search(term = nil, search_fields = [], order_by = 'name ASC')
       return order(order_by) if term.blank?
 
-      search = search_fields.map do |name, value|
-        value[:unaccent] ? "unaccent(#{name}) ILIKE unaccent(?)" : "#{name} ILIKE ?"
+      search = search_fields.map do |field, value|
+        get_query_string(field, value)
       end.join(' OR ')
 
       where(search, *["%#{term}%"] * search_fields.count).order(order_by)
+    end
+
+    def self.get_query_string(field, value)
+      value[:unaccent] ? "unaccent(#{field}) ILIKE unaccent(?)" : "#{field} ILIKE ?"
     end
   end
 end
