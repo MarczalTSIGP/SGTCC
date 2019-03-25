f31488e8a145b77247454df4ed16a33a3cbcf103
renangabriel27 <renanthompsom@gmail.com>
2019-03-25 15:35:16 -0300

    Fix spacing and translationg

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(*)
     if resource_name == :professor
-      return responsible_root_path if current_professor.role?('Professor responsável')
+      return responsible_root_path if current_professor.role?(I18n.t('enums.roles.responsible'))
     end
     send("#{resource_name.to_s.pluralize}_root_path")
   end
