c95eba3a757e82f30a026c43d63c665068bf83a4
renangabriel27 <renanthompsom@gmail.com>
2019-04-23 18:32:19 -0300

    Fix calendar active link

--- app/helpers/link_helper.rb
+++ app/helpers/link_helper.rb
@@ -1,11 +1,15 @@
 module LinkHelper
+  def active_calendars_link
+    request.fullpath.match?('^(/responsible/calendars)$|calendars/new|calendar/edit')
+  end
+
   def active_activities_tcc_one_link
     is_tcc_one = @calendar && @calendar.tcc == 'one'
-    is_tcc_one && request.fullpath.match('activity')
+    is_tcc_one && request.fullpath.match?('activities')
   end
 
   def active_activities_tcc_two_link
     is_tcc_two = @calendar && @calendar.tcc == 'two'
-    is_tcc_two && request.fullpath.match('activity')
+    is_tcc_two && request.fullpath.match?('activities')
   end
 end
