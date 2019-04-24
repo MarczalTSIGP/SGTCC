acec797f810c8e80e69a1ae32c734ba52017c125
renangabriel27 <renanthompsom@gmail.com>
2019-04-24 12:34:42 -0300

    Add datetimepicker component

--- app/javascript/packs/components.js
+++ app/javascript/packs/components.js
@@ -1,9 +1,11 @@
+import Datetimepicker from '../components/shared/datetimepicker';
 import FlashMessages from '../components/shared/flash_messages';
 import ProfileImagePreview from '../components/shared/registrations/profile_image_preview';
 import Search from '../components/shared/search';
 import VueMarkdownPreview from 'vue-markdown';
 
 const components = {
+  Datetimepicker,
   FlashMessages,
   ProfileImagePreview,
   Search,
