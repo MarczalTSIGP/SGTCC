6e94ea62069199a421a0725420eac3a13523ebec
renangabriel27 <renanthompsom@gmail.com>
2019-05-13 10:35:42 -0300

    Fix activities index feature test

--- spec/features/responsible/activities/activities_index_spec.rb
+++ spec/features/responsible/activities/activities_index_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
 
 describe 'Activity::index', type: :feature, js: true do
-  let!(:activities) { create_list(:activity, 2) }
-
   describe '#index' do
     before do
       responsible = create(:responsible)
@@ -12,32 +10,27 @@ describe 'Activity::index', type: :feature, js: true do
     context 'when shows all activities' do
       it 'shows all activities for tcc one with options' do
         calendar = create(:current_calendar_tcc_one)
-        calendar.activities << activities
+        activity = create(:activity_tcc_one, calendar: calendar)
 
         index_url = responsible_calendar_activities_path(calendar)
         visit index_url
 
-        activities.each do |activity|
-          expect(page).to have_contents([activity.name,
-                                         activity.base_activity_type.name,
-                                         activity.deadline])
-        end
-
+        expect(page).to have_contents([activity.name,
+                                       activity.base_activity_type.name,
+                                       activity.deadline])
         expect(page).to have_selector("a[href='#{index_url}'].active")
       end
 
       it 'shows all activities for tcc two with options' do
         calendar = create(:current_calendar_tcc_two)
-        calendar.activities << activities
+        activity = create(:activity_tcc_one, calendar: calendar)
 
         index_url = responsible_calendar_activities_path(calendar)
         visit index_url
 
-        activities.each do |activity|
-          expect(page).to have_contents([activity.name,
-                                         activity.base_activity_type.name,
-                                         activity.deadline])
-        end
+        expect(page).to have_contents([activity.name,
+                                       activity.base_activity_type.name,
+                                       activity.deadline])
         expect(page).to have_selector("a[href='#{index_url}'].active")
       end
     end
@@ -46,16 +39,14 @@ describe 'Activity::index', type: :feature, js: true do
       it 'shows all activities selected by calendar' do
         calendar = create(:calendar_tcc_one, semester: 1)
         second_calendar = create(:calendar_tcc_one, semester: 2)
-        second_calendar.activities << activities
+        activity = create(:activity_tcc_one, calendar: second_calendar)
 
         visit responsible_calendar_activities_path(calendar)
         selectize(second_calendar.year_with_semester, from: 'activity_calendar')
 
-        activities.each do |activity|
-          expect(page).to have_contents([activity.name,
-                                         activity.base_activity_type.name,
-                                         activity.deadline])
-        end
+        expect(page).to have_contents([activity.name,
+                                       activity.base_activity_type.name,
+                                       activity.deadline])
       end
     end
 
@@ -63,16 +54,14 @@ describe 'Activity::index', type: :feature, js: true do
       it 'shows all activities by the next calendar' do
         calendar = create(:current_calendar_tcc_one, semester: 1)
         second_calendar = create(:current_calendar_tcc_one, semester: 2)
-        second_calendar.activities << activities
+        activity = create(:activity_tcc_one, calendar: second_calendar)
 
         visit responsible_calendar_activities_path(calendar)
         find('#next_calendar').click
 
-        activities.each do |activity|
-          expect(page).to have_contents([activity.name,
-                                         activity.base_activity_type.name,
-                                         activity.deadline])
-        end
+        expect(page).to have_contents([activity.name,
+                                       activity.base_activity_type.name,
+                                       activity.deadline])
       end
     end
   end
