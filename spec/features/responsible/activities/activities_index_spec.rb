c8aa8c689ed968c94d95c7c1dedd15c5f6140eb0
renangabriel27 <renanthompsom@gmail.com>
2019-05-13 07:22:34 -0300

    Create activities before the index feature test

--- spec/features/responsible/activities/activities_index_spec.rb
+++ spec/features/responsible/activities/activities_index_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 describe 'Activity::index', type: :feature, js: true do
+  let!(:activities) { create_list(:activity, 2) }
+
   describe '#index' do
     before do
       responsible = create(:responsible)
@@ -10,11 +12,12 @@ describe 'Activity::index', type: :feature, js: true do
     context 'when shows all activities' do
       it 'shows all activities for tcc one with options' do
         calendar = create(:current_calendar_tcc_one)
+        calendar.activities << activities
 
         index_url = responsible_calendar_activities_path(calendar)
         visit index_url
 
-        calendar.activities.each do |activity|
+        activities.each do |activity|
           expect(page).to have_contents([activity.name,
                                          activity.base_activity_type.name,
                                          activity.deadline])
@@ -25,11 +28,12 @@ describe 'Activity::index', type: :feature, js: true do
 
       it 'shows all activities for tcc two with options' do
         calendar = create(:current_calendar_tcc_two)
+        calendar.activities << activities
 
         index_url = responsible_calendar_activities_path(calendar)
         visit index_url
 
-        calendar.activities.each do |activity|
+        activities.each do |activity|
           expect(page).to have_contents([activity.name,
                                          activity.base_activity_type.name,
                                          activity.deadline])
@@ -42,11 +46,12 @@ describe 'Activity::index', type: :feature, js: true do
       it 'shows all activities selected by calendar' do
         calendar = create(:calendar_tcc_one, semester: 1)
         second_calendar = create(:calendar_tcc_one, semester: 2)
+        second_calendar.activities << activities
 
         visit responsible_calendar_activities_path(calendar)
         selectize(second_calendar.year_with_semester, from: 'activity_calendar')
 
-        second_calendar.activities.each do |activity|
+        activities.each do |activity|
           expect(page).to have_contents([activity.name,
                                          activity.base_activity_type.name,
                                          activity.deadline])
@@ -58,11 +63,12 @@ describe 'Activity::index', type: :feature, js: true do
       it 'shows all activities by the next calendar' do
         calendar = create(:current_calendar_tcc_one, semester: 1)
         second_calendar = create(:current_calendar_tcc_one, semester: 2)
+        second_calendar.activities << activities
 
         visit responsible_calendar_activities_path(calendar)
         find('#next_calendar').click
 
-        second_calendar.activities.each do |activity|
+        activities.each do |activity|
           expect(page).to have_contents([activity.name,
                                          activity.base_activity_type.name,
                                          activity.deadline])
