b6e6670ae5b9874a8d05a6c143a4c9a76f33f3bf
renangabriel27 <renanthompsom@gmail.com>
2019-05-18 11:35:07 -0300

    Add current activities

---
+++ spec/features/academics/activities/activities_index_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+describe 'Activity::index', type: :feature, js: true do
+  describe '#index' do
+    before do
+      academic = create(:academic)
+      login_as(academic, scope: :academic)
+    end
+
+    context 'when shows all activities' do
+      it 'shows all activities for tcc one with options' do
+        calendar = create(:current_calendar_tcc_one)
+        activity = create(:activity_tcc_one, calendar: calendar)
+
+        index_url = academics_calendar_activities_path(calendar)
+        visit index_url
+
+        expect(page).to have_contents([activity.name,
+                                       activity.base_activity_type.name,
+                                       I18n.t("enums.tcc.#{activity.tcc}"),
+                                       activity.deadline])
+        expect(page).to have_selector("a[href='#{index_url}'].active")
+      end
+
+      it 'shows all activities for tcc two with options' do
+        calendar = create(:current_calendar_tcc_two)
+        activity = create(:activity_tcc_one, calendar: calendar)
+
+        index_url = academics_calendar_activities_path(calendar)
+        visit index_url
+
+        expect(page).to have_contents([activity.name,
+                                       activity.base_activity_type.name,
+                                       I18n.t("enums.tcc.#{activity.tcc}"),
+                                       activity.deadline])
+        expect(page).to have_selector("a[href='#{index_url}'].active")
+      end
+    end
+  end
+end
