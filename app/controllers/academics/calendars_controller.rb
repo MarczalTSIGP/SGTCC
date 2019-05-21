6c34ca78ab85aed37c63cba197770f618f2bd632
renangabriel27 <renanthompsom@gmail.com>
2019-05-21 16:46:54 -0300

    Fix academics breadcrumbs

--- app/controllers/academics/calendars_controller.rb
+++ app/controllers/academics/calendars_controller.rb
@@ -1,4 +1,7 @@
 class Academics::CalendarsController < Academics::BaseController
+  add_breadcrumb I18n.t('breadcrumbs.calendars.index'),
+                 :academics_calendars_path
+
   def index
     calendars = current_academic.orientations
                                 .includes(:calendar)
