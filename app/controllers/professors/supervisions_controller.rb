d94f145d4714b3c1c5c36a374785849d0723cfc0
renangabriel27 <renanthompsom@gmail.com>
2019-05-27 12:59:28 -0300

    Fix professors/supervisions breadcrumbs

--- app/controllers/professors/supervisions_controller.rb
+++ app/controllers/professors/supervisions_controller.rb
@@ -1,13 +1,14 @@
 class Professors::SupervisionsController < Professors::BaseController
-  before_action :set_tcc_one_title, only: :tcc_one
-  before_action :set_tcc_two_title, only: :tcc_two
   before_action :set_orientations
   before_action :set_orientation, only: :show
+  before_action :set_index_breadcrumb, only: :show
 
   add_breadcrumb I18n.t('breadcrumbs.supervisions.index'),
-                 :professors_supervisions_tcc_one_path
+                 :professors_supervisions_tcc_one_path,
+                 only: [:tcc_one, :tcc_two]
 
   def tcc_one
+    @title = calendar_title
     @search_url = professors_supervisions_search_tcc_one_path
     @orientations = paginate_orientations(@orientations.current_tcc_one)
 
@@ -15,6 +16,7 @@ class Professors::SupervisionsController < Professors::BaseController
   end
 
   def tcc_two
+    @title = calendar_title('two')
     @search_url = professors_supervisions_search_tcc_two_path
     @orientations = paginate_orientations(@orientations.current_tcc_two)
 
@@ -25,7 +27,12 @@ class Professors::SupervisionsController < Professors::BaseController
     @orientations = paginate_orientations(@orientations)
   end
 
-  def show; end
+  def show
+    calendar = @orientation.calendar
+    add_breadcrumb I18n.t("breadcrumbs.supervisions.tcc.#{calendar.tcc}.show",
+                          calendar: calendar.year_with_semester),
+                   professors_supervision_path(@orientation)
+  end
 
   private
 
@@ -42,15 +49,22 @@ class Professors::SupervisionsController < Professors::BaseController
     @orientation = current_professor.supervisions.find(params[:id])
   end
 
-  def set_tcc_one_title
-    @title = calendar_title
-  end
-
-  def set_tcc_two_title
-    @title = calendar_title('two')
-  end
-
   def paginate_orientations(data)
     Orientation.paginate_array(Orientation.search(params[:term], data), params[:page])
+  end
+
+  def set_index_breadcrumb
+    calendar = @orientation.calendar
+    calendar_title = I18n.t("breadcrumbs.supervisions.tcc.#{calendar.tcc}.calendar",
+                            calendar: calendar.year_with_semester)
+    current_calendar = (Calendar.current_by_tcc_one?(calendar) ||
+      Calendar.current_by_tcc_two?(calendar))
+    return add_breadcrumb calendar_title, current_tcc_index_link if current_calendar
+    add_breadcrumb I18n.t('breadcrumbs.supervisions.history'), professors_supervisions_history_path
+  end
+
+  def current_tcc_index_link
+    return professors_supervisions_tcc_one_path if @orientation.calendar.tcc == 'one'
+    professors_supervisions_tcc_two_path
   end
 end
