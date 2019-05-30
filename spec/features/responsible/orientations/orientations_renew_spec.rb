a1d2e0131ba17d72456f9e907866981b1073f133
renangabriel27 <renanthompsom@gmail.com>
2019-05-30 18:25:15 -0300

    Add i18n to renew feature test

--- spec/features/responsible/orientations/orientations_renew_spec.rb
+++ spec/features/responsible/orientations/orientations_renew_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 describe 'Orientation::renew', type: :feature do
   let(:responsible) { create(:responsible) }
-  let(:current_calendar_tcc_two) { create(:current_calendar_tcc_two) }
-  let(:current_orientation_tcc_two) { create(:orientation, calendar: current_calendar_tcc_two) }
+  let(:calendar) { create(:calendar_tcc_two, semester: 1) }
+  let(:current_orientation_tcc_two) { create(:orientation, calendar: calendar) }
   let(:resource_name) { Orientation.model_name.human }
 
   before do
@@ -14,19 +14,13 @@ describe 'Orientation::renew', type: :feature do
   describe '#show', js: true do
     context 'when the renew the tcc two orientation is valid' do
       before do
-        calendar = current_calendar_tcc_two
-        year = calendar.year
-        if calendar.semester == 'one'
-          create(:calendar_tcc_two, semester: 2, year: year)
-        else
-          create(:calendar_tcc_two, semester: 1, year: year.to_i + 1)
-        end
+        create(:calendar_tcc_two, semester: 2, year: calendar.year)
       end
 
       it 'show success message and update the status' do
-        find('button[id="renew_justification"]', text: 'Renovar orientação').click
-        fill_in 'orientation_renewal_justification', with: 'Teste'
-        find('button[id="save_justification"]', text: 'Salvar').click
+        find('button[id="renew_justification"]', text: orientation_renew_button).click
+        fill_in 'orientation_renewal_justification', with: 'Justification'
+        find('button[id="save_justification"]', text: save_button).click
         expect(page).to have_flash(:success, text: message('update.f'))
         current_orientation_tcc_two.reload
         status = Orientation.statuses[current_orientation_tcc_two.status]
@@ -36,16 +30,16 @@ describe 'Orientation::renew', type: :feature do
 
     context 'when the renew the tcc two orientation is invalid' do
       it 'show blank error message' do
-        find('button[id="renew_justification"]', text: 'Renovar orientação').click
-        find('button[id="save_justification"]', text: 'Salvar').click
+        find('button[id="renew_justification"]', text: orientation_renew_button).click
+        find('button[id="save_justification"]', text: save_button).click
         expect(page).to have_message(blank_error_message,
                                      in: 'div.orientation_renewal_justification')
       end
 
       it 'show calendar error message' do
-        find('button[id="renew_justification"]', text: 'Renovar orientação').click
-        fill_in 'orientation_renewal_justification', with: 'Teste'
-        find('button[id="save_justification"]', text: 'Salvar').click
+        find('button[id="renew_justification"]', text: orientation_renew_button).click
+        fill_in 'orientation_renewal_justification', with: 'Justification'
+        find('button[id="save_justification"]', text: save_button).click
         expect(page).to have_message(orientation_renew_calendar_error_message,
                                      in: 'div.orientation_renewal_justification')
       end
