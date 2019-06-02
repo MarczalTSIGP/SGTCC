83a12a9782a6e12fef9e306b02071ae90aa6a05a
renangabriel27 <renanthompsom@gmail.com>
2019-06-02 13:02:55 -0300

    Refactor module orientation cancel

--- app/controllers/concerns/orientation_cancel.rb
+++ app/controllers/concerns/orientation_cancel.rb
@@ -2,15 +2,34 @@ module OrientationCancel
   extend ActiveSupport::Concern
 
   def cancel
-    orientation = Orientation.find(params[:id])
-    orientation.cancel
-    status = orientation.status
+    if orientation.cancel(justification)
+      success_json_message(orientation.status)
+    else
+      error_json_message
+    end
+  end
 
+  private
+
+  def success_json_message(status)
     render json: {
       message: I18n.t('json.messages.orientation.cancel.success'),
       orientation: {
         status: { enum: Orientation.statuses[status], label: status }
       }
     }
+  end
+
+  def error_json_message
+    message = I18n.t('json.messages.orientation.cancel.error')
+    render json: { message: message, status: :internal_server_error }
+  end
+
+  def justification
+    params['orientation']['cancellation_justification']
+  end
+
+  def orientation
+    Orientation.find(params[:id])
   end
 end
