62ce67e301d4b9df6967694f3fb8590e4d6570a2
renangabriel27 <renanthompsom@gmail.com>
2019-06-18 00:15:40 -0300

    Cover signature model tests

--- spec/models/signature_spec.rb
+++ spec/models/signature_spec.rb
@@ -5,4 +5,23 @@ RSpec.describe Signature, type: :model do
     it { is_expected.to belong_to(:orientation) }
     it { is_expected.to belong_to(:document) }
   end
+
+  describe '#sign' do
+    let(:signature) { create(:signature) }
+
+    it 'returns the new status of the signature' do
+      signature.sign
+      expect(signature.status).to eq(true)
+    end
+  end
+
+  describe '#by_professor_and_status' do
+    let(:professor) { create(:professor) }
+    let(:signature) { create(:signature, user_id: professor.id) }
+
+    it 'returns the signature by professor and status' do
+      signatures = Signature.where(user_id: professor.id, user_type: 'P', status: false)
+      expect(Signature.by_professor_and_status(professor, false)).to eq(signatures)
+    end
+  end
 end
