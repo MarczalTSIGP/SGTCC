6c1a3abc14572a36193f82879a2ec3294412bbaf
renangabriel27 <renanthompsom@gmail.com>
2019-07-04 14:26:32 -0300

    Add title to signature status table

--- app/javascript/components/signatures/signature_status.vue
+++ app/javascript/components/signatures/signature_status.vue
@@ -1,29 +1,36 @@
 <template>
   <div
     v-show="show"
-    class="table-responsive"
+    class="card"
   >
-    <table class="table table-hover table-outline table-vcenter text-nowrap card-table">
-      <thead>
-        <tr>
-          <th>Usuário</th>
-          <th>Assinou</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr
-          v-for="signature in signatureStatus"
-          :key="signature.name"
-        >
-          <td>{{ signature.name }}</td>
-          <td>
-            <span :class="selectBadge(signature.status)">
-              {{ selectStatus(signature.status) }}
-            </span>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="card-header">
+      <h3 class="card-title">
+        Esse termo deve ser assinado pelos seguintes usuários:
+      </h3>
+    </div>
+    <div class="table-responsive">
+      <table class="table table-hover table-outline table-vcenter text-nowrap card-table">
+        <thead>
+          <tr>
+            <th>Usuário</th>
+            <th>Assinou</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="signature in signatureStatus"
+            :key="signature.name"
+          >
+            <td>{{ signature.name }}</td>
+            <td>
+              <span :class="selectBadge(signature.status)">
+                {{ selectStatus(signature.status) }}
+              </span>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
