f826f39fec80f44a96b564a67a227d5f1f562b7f
renangabriel27 <renanthompsom@gmail.com>
2019-07-08 08:04:02 -0300

    Add signature code all signed? model test

--- spec/models/signature_code_spec.rb
+++ spec/models/signature_code_spec.rb
@@ -9,4 +9,32 @@ RSpec.describe SignatureCode, type: :model do
   describe 'associations' do
     it { is_expected.to have_many(:signatures).dependent(:destroy) }
   end
+
+  describe '#all_signed?' do
+    let(:signature_code) { create(:signature_code) }
+
+    context 'when returns true' do
+      let(:signature_signed) { create(:signature_signed) }
+
+      before do
+        signature_code.signatures << signature_signed
+      end
+
+      it 'returns true' do
+        expect(signature_code.all_signed?).to eq(true)
+      end
+    end
+
+    context 'when returns false' do
+      let(:signature) { create(:signature) }
+
+      before do
+        signature_code.signatures << signature
+      end
+
+      it 'returns false' do
+        expect(signature_code.all_signed?).to eq(false)
+      end
+    end
+  end
 end
