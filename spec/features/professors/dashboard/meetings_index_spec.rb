cd95dfa0b9bd91652710aa72ebcb285b0594efc5
renangabriel27 <renanthompsom@gmail.com>
2019-08-19 18:01:59 -0300

    Add tests for dashboard/meeting table and fix table spacing

---
+++ spec/features/professors/dashboard/meetings_index_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe 'Meeting::index', type: :feature, js: true do
+  let(:professor) { create(:professor) }
+  let!(:meetings) { create_list(:meeting, 3) }
+
+  before do
+    professor.orientations << Orientation.all
+    login_as(professor, scope: :professor)
+    visit professors_root_path
+  end
+
+  describe '#index' do
+    context 'when shows all meetings' do
+      it 'shows all meetings with options' do
+        meetings.each do |meeting|
+          expect(page).to have_contents([meeting.orientation.academic_with_calendar,
+                                         short_date(meeting.date),
+                                         short_date(meeting.updated_at)])
+        end
+      end
+    end
+  end
+end
