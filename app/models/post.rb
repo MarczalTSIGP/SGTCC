0aeb65800697622e7e53ae3b486395e67bb6d556
renangabriel27 <renanthompsom@gmail.com>
2019-09-02 18:22:41 -0300

    After save create sidebar json

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,4 +7,21 @@ class Post < ApplicationRecord
   validates :title, presence: true
   validates :fa_icon, presence: true
   validates :content, presence: true
+
+  after_save do
+    site = Site.first
+    sidebar = site.sidebar
+    if sidebar.present?
+      new_sidebar = sidebar << select_post_object
+      site.update(sidebar: new_sidebar)
+    else
+      site.update(sidebar: [select_post_object(1)])
+    end
+  end
+
+  private
+
+  def select_post_object(order = Post.all.size)
+    { name: title, url: url, icon: fa_icon, order: order }
+  end
 end
