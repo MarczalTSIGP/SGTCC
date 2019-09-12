09dccafee71b3c32954bb78ac1b873525f5efca4
renangabriel27 <renanthompsom@gmail.com>
2019-09-12 17:08:49 -0300

    Add feature test for site professors show

---
+++ spec/features/site/professors/professors_show_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+describe 'Professors::show', type: :feature do
+  let!(:professor) { create(:professor) }
+
+  before do
+    visit site_professor_path(professor)
+  end
+
+  describe '#show' do
+    context 'when shows the professor' do
+      it 'shows the professor' do
+        gender = I18n.t("enums.genders.#{professor.gender}")
+        is_active = I18n.t("helpers.boolean.#{professor.is_active}")
+        available_advisor = I18n.t("helpers.boolean.#{professor.available_advisor}")
+
+        expect(page).to have_contents([professor.name,
+                                       professor.email,
+                                       professor.username,
+                                       professor.lattes,
+                                       gender,
+                                       is_active,
+                                       available_advisor,
+                                       professor.scholarity.name,
+                                       professor.professor_type.name])
+      end
+    end
+  end
+end
