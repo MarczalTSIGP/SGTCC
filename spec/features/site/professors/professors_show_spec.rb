7adc8f95952a15313578f9dc0ae858e260ce1851
renangabriel27 <renanthompsom@gmail.com>
2019-09-13 10:08:53 -0300

    Cover professors page with feature tests

--- spec/features/site/professors/professors_show_spec.rb
+++ spec/features/site/professors/professors_show_spec.rb
@@ -9,7 +9,7 @@ describe 'Professors::show', type: :feature do
 
   describe '#show' do
     context 'when shows the professor' do
-      it 'shows the professor' do
+      it 'show the professor' do
         gender = I18n.t("enums.genders.#{professor.gender}")
         is_active = I18n.t("helpers.boolean.#{professor.is_active}")
         available_advisor = I18n.t("helpers.boolean.#{professor.available_advisor}")
@@ -24,21 +24,57 @@ describe 'Professors::show', type: :feature do
                                        professor.scholarity.name,
                                        professor.professor_type.name])
       end
+    end
 
-      it 'shows the professor in progress orientations' do
-        professor.orientations_by_status('IN_PROGRESS').each do |orientation|
+    context 'when shows the approved orientations' do
+      it 'show the tcc one orientations' do
+        professor.tcc_one_approved_orientations.each do |orientation|
           expect(page).to have_contents([orientation.short_title,
                                          orientation.advisor.name,
                                          orientation.academic.name])
         end
       end
 
-      it 'shows the professor approved orientations' do
-        professor.orientations_by_status('APPROVED').each do |orientation|
+      it 'show the tcc two orientations' do
+        professor.tcc_two_approved_orientations.each do |orientation|
           expect(page).to have_contents([orientation.short_title,
                                          orientation.advisor.name,
                                          orientation.academic.name])
         end
+      end
+    end
+
+    context 'when shows the in progress orientations' do
+      it 'show the tcc one orientations' do
+        professor.tcc_one_in_progress_orientations.each do |orientation|
+          expect(page).to have_contents([orientation.short_title,
+                                         orientation.advisor.name,
+                                         orientation.academic.name])
+        end
+      end
+
+      it 'show the tcc two orientations' do
+        professor.tcc_two_in_progress_orientations.each do |orientation|
+          expect(page).to have_contents([orientation.short_title,
+                                         orientation.advisor.name,
+                                         orientation.academic.name])
+        end
+      end
+    end
+
+    context 'when shows the orientations report' do
+      let(:report) { professor.total_orientations_report }
+      let(:tcc_one_report) { report[:tcc_one] }
+      let(:tcc_two_report) { report[:tcc_two] }
+
+      it 'show the tcc one reports' do
+        expect(page).to have_contents([tcc_one_report[:approved],
+                                       tcc_one_report[:in_progress]])
+      end
+
+      it 'show the tcc two reports' do
+        expect(page).to have_contents([tcc_two_report[:approved],
+                                       tcc_two_report[:in_progress]])
       end
     end
   end
