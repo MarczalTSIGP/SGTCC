6611fc51ebe7d83c2c2b5dd849f3fe352fe60e85
renangabriel27 <renanthompsom@gmail.com>
2019-09-13 12:23:13 -0300

    Add calendar select by years

--- app/javascript/components/site/orientations-table.vue
+++ app/javascript/components/site/orientations-table.vue
@@ -1,51 +1,75 @@
 <template>
-  <div class="site-table table-responsive w-100">
-    <table class="table border border-gray card-table table-striped table-vcenter text-nowrap">
-      <thead>
-        <tr>
-          <th>Acadêmico</th>
-          <th>Título</th>
-          <th>Orientador</th>
-          <th>Coorientadores</th>
-        </tr>
-      </thead>
-
-      <tbody>
-        <tr v-show="noResults">
-          <td
-            colspan="5"
-            class="text-center"
-          >
-            Nenhuma orientação encontrada!
-          </td>
-        </tr>
-        <tr
-          v-for="orientation in orientations"
-          :key="orientation.id"
+  <div>
+    <div class="input-group mb-3">
+      <div class="input-group-prepend">
+        <button class="btn btn-outline-primary">
+          <i class="fe fe-chevrons-left" />
+        </button>
+      </div>
+      <select class="custom-select">
+        <option
+          v-for="(year, index) in years"
+          :key="index"
+          value="year"
         >
-          <td>{{ orientation.academic.name }}</td>
-          <td>
-            <span
-              data-toggle="tooltip"
-              :title="orientation.title"
+          {{ year }}
+        </option>
+      </select>
+      <div class="input-group-append">
+        <button class="btn btn-outline-primary">
+          <i class="fe fe-chevrons-right" />
+        </button>
+      </div>
+    </div>
+
+    <div class="site-table table-responsive w-100">
+      <table class="table border border-gray card-table table-striped table-vcenter text-nowrap">
+        <thead>
+          <tr>
+            <th>Acadêmico</th>
+            <th>Título</th>
+            <th>Orientador</th>
+            <th>Coorientadores</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr v-show="noResults">
+            <td
+              colspan="5"
+              class="text-center"
             >
-              {{ orientation.short_title }}
-            </span>
-          </td>
-          <td>{{ orientation.advisor.name_with_scholarity }}</td>
-          <td>
-            <span
-              v-for="supervisor in orientation.supervisors"
-              :key="supervisor.id"
-            >
-              <p class="m-0">
-                {{ supervisor.name }}
-              </p>
-            </span>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+              Nenhuma orientação encontrada!
+            </td>
+          </tr>
+          <tr
+            v-for="orientation in orientations"
+            :key="orientation.id"
+          >
+            <td>{{ orientation.academic.name }}</td>
+            <td>
+              <span
+                data-toggle="tooltip"
+                :title="orientation.title"
+              >
+                {{ orientation.short_title }}
+              </span>
+            </td>
+            <td>{{ orientation.advisor.name_with_scholarity }}</td>
+            <td>
+              <span
+                v-for="supervisor in orientation.supervisors"
+                :key="supervisor.id"
+              >
+                <p class="m-0">
+                  {{ supervisor.name }}
+                </p>
+              </span>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
@@ -56,6 +80,11 @@ export default {
 
   props: {
     data: {
+      type: Array,
+      required: true
+    },
+
+    years: {
       type: Array,
       required: true
     },
