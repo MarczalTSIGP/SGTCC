42b385e6a79d2bb58932fbc82deec408d3e0df3c
renangabriel27 <renanthompsom@gmail.com>
2019-10-02 12:14:57 -0300

    Fix clone base activities

--- app/services/populate/base_activities.rb
+++ app/services/populate/base_activities.rb
@@ -1,9 +1,11 @@
 class Populate::BaseActivities
-  attr_reader :tccs, :base_activity_type_ids
+  attr_reader :tccs, :base_activity_type_ids, :identifiers, :tcc_one_identifiers
 
   def initialize
     @tccs = BaseActivity.tccs.values
     @base_activity_type_ids = BaseActivityType.pluck(:id)
+    @identifiers = BaseActivity.human_identifiers.values
+    @tcc_one_identifiers = BaseActivity.human_tcc_one_identifiers.values
   end
 
   def populate
@@ -14,10 +16,14 @@ class Populate::BaseActivities
 
   def create_base_activities
     10.times do |index|
+      tcc = @tccs.sample
+
       BaseActivity.create!(
         name: "Atividade base #{index}",
-        tcc: @tccs.sample,
-        base_activity_type_id: @base_activity_type_ids.sample
+        tcc: tcc,
+        base_activity_type_id: @base_activity_type_ids.sample,
+        identifier: tcc == 1 ? @tcc_one_identifiers.sample : @identifiers.last,
+        judgment: Faker::Boolean.boolean
       )
     end
   end
