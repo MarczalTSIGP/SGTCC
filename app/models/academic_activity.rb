b0dcaf6c4b985be9fa6b71a94697b0d2315c6303
renangabriel27 <renanthompsom@gmail.com>
2019-10-02 14:21:37 -0300

    Add pdf uploder and zip uploader

--- app/models/academic_activity.rb
+++ app/models/academic_activity.rb
@@ -1,4 +1,7 @@
 class AcademicActivity < ApplicationRecord
   belongs_to :academic
   belongs_to :activity
+
+  mount_uploader :pdf, PdfUploader
+  mount_uploader :complementary_files, ZipUploader
 end
