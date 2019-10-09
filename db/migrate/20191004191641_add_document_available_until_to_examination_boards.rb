91017256b1ff1d09d8014a858141fa114ac43b7d
renangabriel27 <renanthompsom@gmail.com>
2019-10-09 07:58:02 -0300

    Fix tooltip and document available date default

--- db/migrate/20191004191641_add_document_available_until_to_examination_boards.rb
+++ db/migrate/20191004191641_add_document_available_until_to_examination_boards.rb
@@ -2,4 +2,8 @@ class AddDocumentAvailableUntilToExaminationBoards < ActiveRecord::Migration[5.2
   change_table :examination_boards, bulk: true do |t|
     t.timestamp :document_available_until
   end
+
+  ExaminationBoard.all.each do |examination_board|
+    examination_board.update(document_available_until: examination_board.date)
+  end
 end
