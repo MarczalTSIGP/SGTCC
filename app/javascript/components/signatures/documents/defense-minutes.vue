490548488d5341eaa18385a2ecd6eea38f165285
renangabriel27 <renanthompsom@gmail.com>
2019-10-11 07:53:02 -0300

    Split orientation from examination board evaluators

--- app/javascript/components/signatures/documents/defense-minutes.vue
+++ app/javascript/components/signatures/documents/defense-minutes.vue
@@ -12,6 +12,38 @@
       <thead class="text-center">
         <tr>
           <th colspan="2">
+            Orientação
+          </th>
+        </tr>
+        <tr>
+          <th>Membro</th>
+          <th>Nome</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr>
+          <td width="30%">
+            Orientador
+          </td>
+          <td>{{ term.advisor.name }}</td>
+        </tr>
+        <tr
+          v-for="(supervisor, index) in supervisors"
+          :key="supervisor.id"
+        >
+          <td width="30%">
+            Coorientador {{ index + 1 }}
+          </td>
+          <td>{{ supervisor.name }}</td>
+        </tr>
+      </tbody>
+    </table>
+
+    <table class="table table-vcenter table-bordered">
+      <thead class="text-center">
+        <tr>
+          <th colspan="2">
             Banca Avaliadora
           </th>
         </tr>
@@ -23,22 +55,18 @@
 
       <tbody>
         <tr>
-          <td>Orientador</td>
+          <td width="30%">
+            Orientador
+          </td>
           <td>{{ term.advisor.name }}</td>
         </tr>
-        <tr
-          v-for="(supervisor, index) in supervisors"
-          :key="supervisor.id"
-        >
-          <td>Coorientador {{ index + 1 }}</td>
-          <td>{{ supervisor.name }}</td>
-        </tr>
-        <tr />
         <tr
           v-for="(evaluator, index) in evaluators"
           :key="evaluator.id"
         >
-          <td>Avaliador {{ index + 1 }}</td>
+          <td width="30%">
+            Avaliador {{ index + 1 }}
+          </td>
           <td>{{ evaluator.name }}</td>
         </tr>
       </tbody>
