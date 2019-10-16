2a272c1a580b9dbd128e87984398cd2cdd3ec335
renangabriel27 <renanthompsom@gmail.com>
2019-10-16 12:37:37 -0300

    Update generate defense minutes div style

--- app/javascript/components/examination-boards/generate-non-attendance-defense-minutes.vue
+++ app/javascript/components/examination-boards/generate-non-attendance-defense-minutes.vue
@@ -2,26 +2,53 @@
   <div>
     <div
       v-if="!show"
-      class="clearfix"
+      class="border border-primary rounded p-4"
     >
-      <button
-        id="generate_defense_minutes"
-        class="btn btn-outline-primary float-right mb-4"
-        @click="confirmGenerate()"
-      >
-        Gerar Ata de Defesa de não comparecimento do Acadêmico
-      </button>
+      <div class="m-3">
+        <div class="float-left">
+          <strong>
+            Gerar Ata de Defesa de não comparecimento do Acadêmico
+          </strong>
+          <p>
+            Ata de defesa de não comparecimento do acadêmico deve ser
+            gerada quando o acadêmico não compareceu para defender seu TCC
+          </p>
+        </div>
+        <button
+          id="generate_defense_minutes"
+          type="button"
+          class="btn btn-outline-primary btn-sm float-right"
+          @click="confirmGenerate()"
+        >
+          Gerar Ata de Defesa de não comparecimento
+        </button>
+      </div>
+      <div class="clearfix" />
+      <hr class="m-0">
     </div>
-    <div v-else>
-      <a
-        id="view_defense_minutes"
-        :href="documentUrl"
-        class="btn btn-outline-primary float-right mb-4"
-        :class="isDisabled"
-        :aria-disabled="loading"
-      >
-        Visualizar Ata de Defesa
-      </a>
+    <div
+      v-else
+      class="border border-primary rounded p-4 clearfix"
+    >
+      <div class="m-3">
+        <div class="float-left">
+          <strong>
+            Visualizar Ata de Defesa
+          </strong>
+          <p>
+            Visualizar documento gerado desta banca de defesa.
+          </p>
+        </div>
+        <a
+          id="view_defense_minutes"
+          :href="documentUrl"
+          class="btn btn-outline-primary btn-sm float-right"
+          :class="isDisabled"
+          :aria-disabled="loading"
+        >
+          Visualizar Ata de Defesa
+        </a>
+      </div>
     </div>
   </div>
 </template>
