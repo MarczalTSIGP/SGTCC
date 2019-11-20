02c307cb781a0b34c1ab5317111296c763d710b7
renangabriel27 <renanthompsom@gmail.com>
2019-11-19 21:06:02 -0300

    Create tco and tcai only if not exists

--- app/models/concerns/orientation_documents.rb
+++ app/models/concerns/orientation_documents.rb
@@ -6,14 +6,26 @@ module OrientationDocuments
   included do
     after_save do
       params = { orientation_id: id }
-      DocumentType.find_by(identifier: :tco).documents.create!(params)
-      DocumentType.find_by(identifier: :tcai).documents.create!(params) if institution.present?
+
+      DocumentType.find_by(identifier: :tco).documents.create!(params) unless tco?
+
+      if institution.present? && !tcai?
+        DocumentType.find_by(identifier: :tcai).documents.create!(params)
+      end
     end
 
     after_update do
       document_ids = signatures.pluck(:document_id)
       signatures.destroy_all
       Document.delete(document_ids)
+    end
+
+    def tco?
+      documents.find_by(document_type_id: DocumentType.tco).present?
+    end
+
+    def tcai?
+      documents.find_by(document_type_id: DocumentType.tcai).present?
     end
   end
 end
