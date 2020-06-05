2e4600edd1d3cca34c239722e789c718b27ca708
utfpr-gp-tsi <dmarczal@gmail.com>
2020-06-05 10:01:53 -0300

    Change server IP

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 set :stage, :production
 
-server '172.20.11.14', roles: %w(app web db), primary: true, user: 'deployer'
+server '200.134.18.134', roles: %w(app web db), primary: true, user: 'deployer'
 set :rails_env, "production"
