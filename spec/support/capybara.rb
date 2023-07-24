7a7e86117d12b15e16769e4da0bbe310a45dbd29
Diego Marczal <dmarczal@gmail.com>
2023-07-24 15:07:40 -0300

    Starting fixed tests

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,4 +1,4 @@
-Capybara.server = :puma, { Silent: true }
+# Capybara.server = :puma, { Silent: true }
 
 # Chrome non-headless driver
 Capybara.register_driver :chrome do |app|
@@ -11,10 +11,13 @@ Capybara.register_driver :selenium do |app|
   opts = Selenium::WebDriver::Chrome::Options.new
 
   chrome_args = %w[--headless --no-sandbox --disable-gpu --window-size=1920,1080
-                   --remote-debugging-port=9222]
+                   --disable-dev-shm-usage --remote-debugging-port=9222]
 
   chrome_args.each { |arg| opts.add_argument(arg) }
-  Capybara::Selenium::Driver.new(app, browser: :chrome, options: opts, desired_capabilities: caps)
+  Capybara::Selenium::Driver.new(app,
+    browser: :remote,
+    url: "http://#{ENV['SELENIUM_HOST']}:#{ENV['SELENIUM_PORT']}/wd/hub",
+    options: opts, desired_capabilities: caps)
 end
 
 # Switch between :chrome / :headless_chrome to see tests run in chrome
@@ -27,3 +30,13 @@ else
   Capybara.current_driver = :chrome
   Capybara.javascript_driver = :chrome
 end
+
+def app_host
+  app = "http://#{ENV.fetch('TEST_APP_HOST', nil)}"
+  port = Capybara.current_session.server.port
+  "#{app}:#{port}"
+end
+
+Capybara.disable_animation = true
+Capybara.server_host = '0.0.0.0'
+Capybara.app_host = app_host
