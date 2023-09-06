054fecca58dfd61ab04d6ecab280827df762f684
AmandaCarolyneDeLima <amandaacarolyne@outlook.com>
2023-09-06 13:20:37 -0300

    feat(advisor-area): link added without academic name
    adjusting the test

--- spec/features/professors/orientations/orientations_index_spec.rb
+++ spec/features/professors/orientations/orientations_index_spec.rb
@@ -9,11 +9,12 @@ describe 'Orientation::index', type: :feature do
 
   describe '#index', js: true do
     context 'when shows all the orientations of tcc one calendar' do
-      it 'shows basic information of tcc one supervision' do
-        orientation = create(:current_orientation_tcc_one, advisor: professor)
-        index_url = professors_orientations_tcc_one_path
-        visit index_url
+      let!(:orientation) { create(:current_orientation_tcc_one, advisor: professor) }
+      let(:index_url) { professors_orientations_tcc_one_path }
 
+      before { visit index_url }
+
+      it 'shows basic information of tcc one supervision' do
         expect(page).to have_content(orientation.short_title)
         expect(page).to have_content(orientation.advisor.name)
         expect(page).to have_link(
@@ -23,57 +24,64 @@ describe 'Orientation::index', type: :feature do
       end
 
       it 'shows calendar information of tcc one supervision' do
-        orientation = create(:current_orientation_tcc_one, advisor: professor)
-        index_url = professors_orientations_tcc_one_path
-        visit index_url
-
         orientation.calendars.each do |calendar|
           expect(page).to have_content(calendar.year_with_semester_and_tcc)
         end
       end
 
       it 'shows active link for tcc one supervision' do
-        create(:current_orientation_tcc_one, advisor: professor)
-        index_url = professors_orientations_tcc_one_path
-        visit index_url
-
         expect(page).to have_selector("a[href='#{index_url}'].active")
       end
 
-      it 'clicks on academic name and shows additional options' do
-        create(:current_orientation_tcc_one, advisor: professor)
-        index_url = professors_orientations_tcc_one_path
-        visit index_url
-
+      it 'clicks on academic name and shows "Detalhes da orientação" link' do
         find('.academic-name-link').click
+
         expect(page).to have_link(
           'Detalhes da orientação',
-          href: professors_orientation_path(Orientation.last)
+          href: professors_orientation_path(orientation)
         )
+      end
+
+      it 'clicks on academic name and shows "Visualizar atividades" link' do
+        find('.academic-name-link').click
+
         expect(page).to have_link(
           'Visualizar atividades',
           href: professors_orientation_calendar_activities_path(
-            Orientation.last, Orientation.last.current_calendar
+            orientation, orientation.current_calendar
           )
         )
+      end
+
+      it 'clicks on academic name and shows "Visualizar documentos" link' do
+        find('.academic-name-link').click
+
         expect(page).to have_link(
           'Visualizar documentos',
-          href: professors_orientation_documents_path(Orientation.last)
+          href: professors_orientation_documents_path(orientation)
         )
+      end
 
-        if Orientation.last.meetings.any?
+      it 'clicks on academic name and shows "Visualizar reuniões" link if available' do
+        find('.academic-name-link').click
+
+        if orientation.meetings.any?
           expect(page).to have_link(
             'Visualizar reuniões',
-            href: professors_orientation_meetings_path(Orientation.last)
+            href: professors_orientation_meetings_path(orientation)
           )
         else
           expect(page).not_to have_link('Visualizar reuniões')
         end
+      end
 
-        if Orientation.last.can_be_edited?
+      it 'clicks on academic name and shows "Editar" link if allowed' do
+        find('.academic-name-link').click
+
+        if orientation.can_be_edited?
           expect(page).to have_link(
             'Editar',
-            href: edit_professors_orientation_path(Orientation.last)
+            href: edit_professors_orientation_path(orientation)
           )
         else
           expect(page).not_to have_link('Editar')
--- spec/features/responsible/orientations/orientations_index_spec.rb
+++ spec/features/responsible/orientations/orientations_index_spec.rb
@@ -8,87 +8,129 @@ describe 'Orientation::index', type: :feature do
 
   describe '#index', js: true do
     context 'when showing all the orientations of TCC one calendar' do
-      it 'shows all the orientations of TCC one with options' do
-        create_list(:orientation_tcc_one, 2)
-        orientations = Orientation.includes(:calendars, :academic).recent
-        index_url = responsible_orientations_tcc_one_path
+      let!(:orientations) { create_list(:orientation_tcc_one, 2) }
+      let(:index_url) { responsible_orientations_tcc_one_path }
+
+      before do
         visit index_url
+        sleep 1
+      end
 
-        orientations.each_with_index do |orientation, index|
-          pos = index + 1
-          within("table tbody tr:nth-child(#{pos})") do
-            expect(page).to have_link(
-              orientation.academic.name,
-              href: responsible_orientation_path(orientation)
-            )
-            expect(page).to have_content(orientation.academic.ra)
-            expect(page).to have_content(orientation.short_title)
-            expect(page).to have_content(orientation.advisor.name)
+      it 'displays basic orientation information' do
+        orientations.each do |orientation|
+          academic_link = find("a[href='#{responsible_orientation_path(orientation)}']")
+          expect(academic_link).to have_text(orientation.academic.name)
+        end
+      end
 
-            orientation.calendars.each do |calendar|
-              expect(page).to have_content(
-                calendar.year_with_semester_and_tcc
-              )
-            end
-
-            page.execute_script('arguments[0].click();', find('.academic-name-link'))
-            expect(page).to have_link(
-              'Detalhes da orientação',
-              href: responsible_orientation_path(orientation)
-            )
-            expect(page).to have_link(
-              'Atividades da orientação',
-              href: responsible_orientation_calendar_activities_path(
-                orientation, orientation.current_calendar
-              )
-            )
-
-            if orientation.meetings.any?
-              expect(page).to have_link(
-                'Reuniões da orientação',
-                href: professors_orientation_meetings_path(orientation)
-              )
-            else
-              expect(page).not_to have_link('Reuniões da orientação')
-            end
-
-            unless orientation.documents.empty?
-              expect(page).to have_link(
-                'Documentos da orientação',
-                href: responsible_orientation_documents_path(orientation)
-              )
-            end
-
-            if orientation.can_be_edited? || orientation.can_be_destroyed?
-              expect(page).to have_selector('.dropdown-divider')
-            end
-
-            if orientation.can_be_edited?
-              expect(page).to have_link(
-                'Editar',
-                href: edit_responsible_orientation_path(orientation)
-              )
-            end
-
-            if orientation.can_be_destroyed?
-              expect(page).to have_link(
-                'Remover',
-                href: responsible_orientation_path(orientation)
-              )
-            end
+      it 'displays calendar information' do
+        orientations.each do |orientation|
+          orientation.calendars.each do |calendar|
+            expect(page).to have_content(calendar.year_with_semester_and_tcc)
           end
         end
+      end
 
-        expect(page).to have_selector("a[href='#{index_url}'].active")
+      it 'displays links to orientation details' do
+        orientations.each do |orientation|
+          orientation_link = "a[href='#{responsible_orientation_path(orientation)}']"
+          visit index_url
+          find(orientation_link).click
+
+          expect(page).to have_link(
+            'Detalhes da orientação',
+            href: responsible_orientation_path(orientation)
+          )
+        end
+      end
+
+      it 'displays links to orientation activities' do
+        orientations.each do |orientation|
+          orientation_link = "a[href='#{responsible_orientation_path(orientation)}']"
+          visit index_url
+          find(orientation_link).click
+
+          expect(page).to have_link(
+            'Atividades da orientação',
+            href: responsible_orientation_calendar_activities_path(
+              orientation, orientation.current_calendar
+            )
+          )
+        end
+      end
+
+      it 'displays links to orientation meetings if they exist' do
+        orientations.each do |orientation|
+          orientation_link = "a[href='#{responsible_orientation_path(orientation)}']"
+          visit index_url
+          find(orientation_link).click
+
+          next unless orientation.meetings.any?
+
+          expect(page).to have_link(
+            'Reuniões da orientação',
+            href: professors_orientation_meetings_path(orientation)
+          )
+        end
+      end
+
+      it 'displays links to orientation documents if they exist' do
+        orientations.each do |orientation|
+          orientation_link = "a[href='#{responsible_orientation_path(orientation)}']"
+          visit index_url
+          find(orientation_link).click
+
+          next if orientation.documents.empty?
+
+          expect(page).to have_link(
+            'Documentos da orientação',
+            href: responsible_orientation_documents_path(orientation)
+          )
+        end
+      end
+
+      it 'displays links to edit orientations' do
+        orientations.each do |orientation|
+          orientation_link = "a[href='#{responsible_orientation_path(orientation)}']"
+          visit index_url
+          find(orientation_link).click
+
+          next unless orientation.can_be_edited?
+
+          expect(page).to have_link(
+            'Editar',
+            href: edit_responsible_orientation_path(orientation)
+          )
+        end
+      end
+
+      it 'displays links to remove orientations' do
+        orientations.each do |orientation|
+          orientation_link = "a[href='#{responsible_orientation_path(orientation)}']"
+          visit index_url
+          find(orientation_link).click
+
+          next unless orientation.can_be_destroyed?
+
+          expect(page).to have_link(
+            'Remover',
+            href: responsible_orientation_path(orientation)
+          )
+        end
       end
     end
 
     context 'when showing all the orientations of TCC two calendar' do
-      it 'shows all the orientations of TCC two with options' do
+      let(:index_url) { responsible_orientations_tcc_two_path }
+
+      before do
         create_list(:orientation_tcc_two, 2)
-        orientations = Orientation.includes(:calendars, :academic).recent
-        index_url = responsible_orientations_tcc_two_path
+
         visit index_url
+      end
+
+      it 'displays basic orientation information' do
+        orientations = Orientation.includes(:academic, :calendars).recent
 
         orientations.each_with_index do |orientation, index|
           pos = index + 1
@@ -105,8 +147,6 @@ describe 'Orientation::index', type: :feature do
             end
           end
         end
-
-        expect(page).to have_selector("a[href='#{index_url}'].active")
       end
     end
   end
