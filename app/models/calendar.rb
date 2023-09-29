b565971ee14a6117cc9fb02066423058b59feafc
Ligia Falcao <lih.falcao@hotmail.com>
2023-09-29 20:07:34 -0300

    #198 - Fixed database populate | Divide -> populate the calendars and calendar activities correctly

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -113,15 +113,28 @@ class Calendar < ApplicationRecord
 
   def clone_base_activities
     base_activities = BaseActivity.where(tcc: tcc)
-    base_activities.each { |base_activity| create_activity(base_activity) }
+    interval =  tcc == 1  ? 10.days : 30.days
+    month = Calendar.current_semester == 'one' ? 'mar' : 'aug'
+    initial_date = Time.zone.parse("#{month} 01 00:00:00 #{Calendar.current_year}")
+    final_date = initial_date + interval  + 23.hours + 59.minutes
+    base_activities.each do |base_activity| 
+      create_activity(base_activity, initial_date, final_date) 
+      initial_date += interval + 1.days
+      final_date = initial_date + interval + 23.hours + 59.minutes
+    end
   end
 
-  def create_activity(activity)
-    current_time = Time.current
-    activities.create(name: activity.name, tcc: activity.tcc,
-                      calendar_id: id, base_activity_type_id: activity.base_activity_type_id,
-                      judgment: activity&.judgment, identifier: activity&.identifier,
-                      initial_date: current_time, final_date: current_time,
-                      final_version: activity&.final_version)
+  def create_activity(activity, initial_date, final_date)
+    activities.create(
+      name: activity.name,
+      tcc: activity.tcc,
+      calendar_id: id,
+      base_activity_type_id: activity.base_activity_type_id,
+      judgment: activity&.judgment,
+      identifier: activity&.identifier,
+      initial_date: initial_date,
+      final_date: final_date,
+      final_version: activity&.final_version
+      )
   end
 end
--- app/services/populate/base_activities.rb
+++ app/services/populate/base_activities.rb
@@ -2,7 +2,7 @@ class Populate::BaseActivities
   attr_reader :tccs, :base_activity_type_ids, :identifiers, :tcc_one_identifiers
 
   def initialize
-    @tccs = BaseActivity.tccs.values
+    @tccs = BaseActivity.tccs
     @base_activity_type_ids = BaseActivityType.pluck(:id)
     @identifiers = BaseActivity.human_tcc_identifiers.values
     @tcc_one_identifiers = BaseActivity.human_tcc_one_identifiers.values
@@ -15,16 +15,79 @@ class Populate::BaseActivities
   private
 
   def create_base_activities
-    10.times do |index|
-      tcc = @tccs.sample
+      tcc1 = @tccs['one']
+      tcc2 = @tccs['two']
+
 
       BaseActivity.create!(
-        name: "Atividade base #{index}",
-        tcc: tcc,
+        name: "Assinatura do Termo de Compromisso de Orientação",
+        tcc: tcc1,
         base_activity_type_id: @base_activity_type_ids.sample,
-        identifier: tcc == 1 ? @tcc_one_identifiers.sample : @identifiers.last,
+        identifier: @tcc_one_identifiers.sample,
         judgment: Faker::Boolean.boolean
       )
-    end
+      BaseActivity.create!(
+        name: "Envio da Proposta",
+        tcc: tcc1,
+        base_activity_type_id: @base_activity_type_ids.sample,
+        identifier: @tcc_one_identifiers.sample,
+        judgment: Faker::Boolean.boolean
+      )
+      BaseActivity.create!(
+        name: "Defesa da Proposta",
+        tcc: tcc1,
+        base_activity_type_id: @base_activity_type_ids.sample,
+        identifier: @tcc_one_identifiers.sample,
+        judgment: Faker::Boolean.boolean
+      )
+      BaseActivity.create!(
+        name: "Envio da Versão Final da Proposta",
+        tcc: tcc1,
+        base_activity_type_id: @base_activity_type_ids.sample,
+        identifier: @tcc_one_identifiers.sample,
+        judgment: Faker::Boolean.boolean
+      )
+      BaseActivity.create!(
+        name: "Envio do Projeto",
+        tcc: tcc1,
+        base_activity_type_id: @base_activity_type_ids.sample,
+        identifier: @tcc_one_identifiers.sample,
+        judgment: Faker::Boolean.boolean
+      )
+      BaseActivity.create!(
+        name: "Defesa do Projeto",
+        tcc: tcc1,
+        base_activity_type_id: @base_activity_type_ids.sample,
+        identifier: @tcc_one_identifiers.sample,
+        judgment: Faker::Boolean.boolean
+      )
+      BaseActivity.create!(
+        name: "Envio da Versão Final do Projeto",
+        tcc: tcc1,
+        base_activity_type_id: @base_activity_type_ids.sample,
+        identifier: @tcc_one_identifiers.sample,
+        judgment: Faker::Boolean.boolean
+      )
+      BaseActivity.create!(
+        name: "Envio da Monografia",
+        tcc: tcc2,
+        base_activity_type_id: @base_activity_type_ids.sample,
+        identifier: @identifiers.last,
+        judgment: Faker::Boolean.boolean
+      )
+      BaseActivity.create!(
+        name: "Defesa da Monografia",
+        tcc: tcc2,
+        base_activity_type_id: @base_activity_type_ids.sample,
+        identifier: @identifiers.last,
+        judgment: Faker::Boolean.boolean
+      )
+      BaseActivity.create!(
+        name: "Envio da Versão Final da Monografia",
+        tcc: tcc2,
+        base_activity_type_id: @base_activity_type_ids.sample,
+        identifier: @identifiers.last,
+        judgment: Faker::Boolean.boolean
+      )
   end
 end
