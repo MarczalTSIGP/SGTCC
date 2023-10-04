61bd75d1f1bc4d0c8fe59728f4e53ab46e885050
Diego Marczal <dmarczal@gmail.com>
2023-10-04 18:10:10 -0300

    Removed commented code from the test

--- spec/features/professors/supervisions/supervisions_ebs_index_spec.rb
+++ spec/features/professors/supervisions/supervisions_ebs_index_spec.rb
@@ -26,14 +26,6 @@ describe 'Supervision::ExaminationBoarsIndex', type: :feature do
       expect(page).to have_contents([examination_board.orientation.advisor.name_with_scholarity,
                                      examination_board.place,
                                      datetime(examination_board.date)])
-
-      # examination_board.professors.each do |professor|
-      #   expect(page).to have_content(professor.name_with_scholarity)
-      # end
-      #
-      # examination_board.external_members.each do |external_member|
-      #   expect(page).to have_content(external_member.name_with_scholarity)
-      # end
     end
   end
 end
