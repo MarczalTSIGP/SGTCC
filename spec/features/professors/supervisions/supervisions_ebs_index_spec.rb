8ceb881a0d3dae33e4834bace14d93639a9b1b80
Prof. Diego Marczal <37582258+MarczalTSIGP@users.noreply.github.com>
2023-10-04 18:42:23 -0300

    Fixed the professor supervion examination boards index (#225)
    * Fixed the professor supervion examination boards index
    
    * Removed commented code from the test
    
    ---------
    
    Co-authored-by: Diego Marczal <dmarczal@gmail.com>

---
+++ spec/features/professors/supervisions/supervisions_ebs_index_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe 'Supervision::ExaminationBoarsIndex', type: :feature do
+  let(:professor) { create(:professor) }
+
+  before do
+    login_as(professor, scope: :professor)
+  end
+
+  context 'when shows all the supervisions examinations board' do
+    let!(:examination_board) { create(:examination_board_tcc_one, orientation: orientation) }
+
+    let(:orientation) { create(:current_orientation_tcc_one) }
+    let(:index_url) { professors_supervision_examination_boards_path }
+
+    before do
+      orientation.professor_supervisors << professor
+      visit index_url
+    end
+
+    it 'shows the examination boards of the tcc one with options' do
+      expect_path = professors_supervision_examination_board_path(examination_board)
+      expect(page).to have_link(examination_board.orientation.academic_with_calendar,
+                                href: expect_path)
+
+      expect(page).to have_contents([examination_board.orientation.advisor.name_with_scholarity,
+                                     examination_board.place,
+                                     datetime(examination_board.date)])
+    end
+  end
+end
