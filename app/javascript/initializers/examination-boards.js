db6b8bbc8d2dd84691e3bd3dfcc8b55debf0591f
guilhermeferreira7 <guilhermefribas@outlook.com>
2023-10-05 21:25:57 -0300

    button to show/hide details for examination boards

--- app/javascript/initializers/examination-boards.js
+++ app/javascript/initializers/examination-boards.js
@@ -5,14 +5,14 @@ export default {
 
   methods: {
     initExaminationBoardsHelper() {
-      document.querySelectorAll('.show-eb-details').forEach(function(element) {
-        element.addEventListener('click', function(event) {
+      document.querySelectorAll(".show-eb-details").forEach(function(element) {
+        element.addEventListener("click", function(event) {
           const ebId = this.dataset.ebId;
-          const el = document.getElementById('eb-' + ebId);
-          if (el.classList.contains('d-none')) {
-            el.classList.remove('d-none');
+          const el = document.getElementById("eb-" + ebId);
+          if (el.classList.contains("d-none")) {
+            el.classList.remove("d-none");
           } else {
-            el.classList.add('d-none');
+            el.classList.add("d-none");
           }
           event.preventDefault();
         });
