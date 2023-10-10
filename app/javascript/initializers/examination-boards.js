c44cb78a3304dad852dd3efed08746eb7e410e6a
AmandaCarolyneDeLima <amandaacarolyne@outlook.com>
2023-10-09 22:59:01 -0300

    feat(public-area): improved the layout
    improved presentation layout of examination boards

---
+++ app/javascript/initializers/examination-boards.js
@@ -0,0 +1,40 @@
+export default {
+  mounted() {
+    this.initExaminationBoardsHelper();
+  },
+
+  methods: {
+    initExaminationBoardsHelper() {
+      document
+        .querySelectorAll(".examination-board-date")
+        .forEach(function(element) {
+          const date = new Date(element.dataset.date);
+          const parent = element.parentElement.parentElement;
+          if (date.getTime() < new Date().getTime()) {
+            parent.classList.add("opacity-50");
+          }
+        });
+
+      document
+        .querySelectorAll(".examination-details")
+        .forEach(function(element) {
+          element.classList.add("hidden");
+        });
+
+      document.querySelectorAll(".showDetails").forEach(function(element) {
+        element.addEventListener("click", function() {
+          const examId = element.dataset.examId;
+          document
+            .querySelectorAll(".examination-details.exam_" + examId)
+            .forEach(function(element) {
+              if (element.classList.contains("hidden")) {
+                element.classList.remove("hidden");
+              } else {
+                element.classList.add("hidden");
+              }
+            });
+        });
+      });
+    }
+  }
+};
--- spec/features/site/examination_boards/examination_boards_index_spec.rb
+++ spec/features/site/examination_boards/examination_boards_index_spec.rb
@@ -21,23 +21,48 @@ describe 'ExaminationBoard::index', type: :feature, js: true do
         visit site_examination_boards_path
       end
 
-      it 'shows all the examination boards of the tcc one with options' do
-        advisor_name = examination_board_tcc_one.orientation.advisor.name_with_scholarity
+      it 'Proposta for TCC1' do
+        click_link('Proposta')
+        expect(page).to have_content('Proposta')
 
+        advisor_name = examination_board_tcc_one.orientation.advisor.name_with_scholarity
+        # expect(page).to have_selector(examination_board_tcc_one.academic_activity&.title)
         expect(page).to have_contents([examination_board_tcc_one.orientation.academic.name,
                                        advisor_name,
                                        examination_board_tcc_one.place,
                                        long_date(examination_board_tcc_one.date)])
 
-        examination_board_tcc_one.professors.each do |professor|
-          expect(page).to have_content(professor.name_with_scholarity)
-        end
+        # examination_board_tcc_one.professors.each do |professor|
+        #   expect(page).to have_content(professor.name_with_scholarity)
+        # end
 
-        examination_board_tcc_one.external_members.each do |external_member|
-          expect(page).to have_content(external_member.name_with_scholarity)
-        end
+        # examination_board_tcc_one.external_members.each do |external_member|
+        #   expect(page).to have_content(external_member.name_with_scholarity)
+        # end
 
-        expect(page).to have_selector(link(academic_activity.pdf.url))
+        # expect(page).to have_selector(link(academic_activity.pdf.url))
+      end
+
+      it 'Projeto for TCC1' do
+        click_link('Projeto')
+        expect(page).to have_content('Projeto')
+
+        advisor_name = examination_board_tcc_one.orientation.advisor.name_with_scholarity
+        # expect(page).to have_selector(examination_board_tcc_one.academic_activity&.title)
+        expect(page).to have_contents([examination_board_tcc_one.orientation.academic.name,
+                                       advisor_name,
+                                       examination_board_tcc_one.place,
+                                       long_date(examination_board_tcc_one.date)])
+
+        # examination_board_tcc_one.professors.each do |professor|
+        #   expect(page).to have_content(professor.name_with_scholarity)
+        # end
+
+        # examination_board_tcc_one.external_members.each do |external_member|
+        #   expect(page).to have_content(external_member.name_with_scholarity)
+        # end
+
+        # expect(page).to have_selector(link(academic_activity.pdf.url))
       end
     end
 
@@ -53,24 +78,26 @@ describe 'ExaminationBoard::index', type: :feature, js: true do
         visit site_examination_boards_path
       end
 
-      it 'shows all the examination boards of the tcc two with options' do
-        advisor_name = examination_board_tcc_two.orientation.advisor.name_with_scholarity
+      it 'shows Monografia for TCC2' do
+        click_link('Monografia')
+        expect(page).to have_content('Monografia')
 
+        advisor_name = examination_board_tcc_two.orientation.advisor.name_with_scholarity
+        # expect(page).to have_selector(examination_board_tcc_two.academic_activity&.title)
         expect(page).to have_contents([examination_board_tcc_two.orientation.academic.name,
                                        advisor_name,
                                        examination_board_tcc_two.place,
                                        long_date(examination_board_tcc_two.date)])
 
-        examination_board_tcc_two.professors.each do |professor|
-          expect(page).to have_content(professor.name_with_scholarity)
-        end
+        # examination_board_tcc_two.professors.each do |professor|
+        #   expect(page).to have_content(professor.name_with_scholarity)
+        # end
 
-        examination_board_tcc_two.external_members.each do |external_member|
-          expect(page).to have_content(external_member.name_with_scholarity)
-        end
+        # examination_board_tcc_two.external_members.each do |external_member|
+        #   expect(page).to have_content(external_member.name_with_scholarity)
+        # end
 
-        expect(page).to have_selectors([link(academic_activity.pdf.url),
-                                        link(academic_activity.complementary_files.url)])
+        # expect(page).to have_selector(link(academic_activity.pdf.url))
       end
     end
   end
