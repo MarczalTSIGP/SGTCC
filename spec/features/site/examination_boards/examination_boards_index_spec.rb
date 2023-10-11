5a3a1a99aa292f2434c4540c5877909caf195244
AmandaCarolyneDeLima <amandaacarolyne@outlook.com>
2023-10-11 11:31:38 -0300

    feat(public-area): improved the layout
    improved presentation layout of examination board-test-error

--- spec/features/site/examination_boards/examination_boards_index_spec.rb
+++ spec/features/site/examination_boards/examination_boards_index_spec.rb
@@ -5,15 +5,15 @@ describe 'ExaminationBoard::index', type: :feature, js: true do
     create(:page, url: 'bancas-de-tcc')
   end
 
-  describe '#index' do
-    let!(:examination_board_tcc_one) { create(:current_examination_board_tcc_one) }
-    let!(:examination_board_tcc_one_project) { create(:current_examination_board_tcc_one_project) }
-    let!(:examination_board_tcc_two) { create(:current_examination_board_tcc_two) }
+  describe 'viewing examination boards' do
+    let(:examination_board_tcc_one) { create(:current_examination_board_tcc_one) }
+    let(:examination_board_tcc_one_project) { create(:current_examination_board_tcc_one_project) }
+    let(:examination_board_tcc_two) { create(:current_examination_board_tcc_two) }
 
     context 'when showing all the examination boards of the TCC one calendar' do
       let(:orientation) { examination_board_tcc_one.orientation }
       let(:academic) { orientation.academic }
-      let!(:academic_activity) do
+      let(:academic_activity) do
         create(:proposal_academic_activity, academic: academic,
                                             calendar: orientation.calendars.first)
       end
@@ -22,75 +22,19 @@ describe 'ExaminationBoard::index', type: :feature, js: true do
         visit site_examination_boards_path
       end
 
-      it 'Proposta for TCC1' do
-        click_link('Proposta')
-        expect(page).to have_content('Proposta')
-
-        within('div#tabContent') do
-          advisor_name = examination_board_tcc_one.orientation.advisor.name_with_scholarity
-
-          expect(page).to have_contents([long_date(examination_board_tcc_one.date),
-                                         examination_board_tcc_one.place,
-                                         examination_board_tcc_one.orientation.academic.name,
-                                         advisor_name])
-        end
-
-        find("a[data-exam-id='#{examination_board_tcc_one.id}']").click
-
-        expect(page).to have_content(examination_board_tcc_one.academic_activity&.title)
-
-        within("div.examination-board-row.exam_#{examination_board_tcc_one.id}") do
-          examination_board_tcc_one.orientation.supervisors.each do |supervisor|
-            expect(page).to have_content(supervisor.name_with_scholarity)
-          end
-
-          examination_board_tcc_one.professors.each do |professor|
-            expect(page).to have_content(professor.name_with_scholarity)
-          end
-
-          if examination_board_tcc_one.academic_activity
-            expect(page).to have_selector("a[href='#{examination_board_tcc_one.academic_activity.pdf.url}']")
-          end
-        end
+      it 'shows Proposta for TCC1' do
+        verify_examination_board(examination_board_tcc_one, 'Proposta')
       end
 
-      it 'Projeto for TCC1' do
-        click_link('Projeto')
-        expect(page).to have_content('Projeto')
-
-        within('div#tabContent') do
-          advisor_name = examination_board_tcc_one_project.orientation.advisor.name_with_scholarity
-
-          expect(page).to have_contents([long_date(examination_board_tcc_one_project.date),
-                                         examination_board_tcc_one_project.place,
-                                         examination_board_tcc_one_project.orientation.academic.name,
-                                         advisor_name])
-        end
-
-        find("a[data-exam-id='#{examination_board_tcc_one_project.id}']").click
-
-        expect(page).to have_content(examination_board_tcc_one_project.academic_activity&.title)
-
-        within("div.examination-board-row.exam_#{examination_board_tcc_one_project.id}") do
-          examination_board_tcc_one_project.orientation.supervisors.each do |supervisor|
-            expect(page).to have_content(supervisor.name_with_scholarity)
-          end
-
-          examination_board_tcc_one_project.professors.each do |professor|
-            expect(page).to have_content(professor.name_with_scholarity)
-          end
-
-          if examination_board_tcc_one_project.academic_activity
-            expect(page).to have_selector("a[href='#{examination_board_tcc_one_project.academic_activity.pdf.url}']")
-          end
-        end
+      it 'shows Projeto for TCC1' do
+        verify_examination_board(examination_board_tcc_one_project, 'Projeto')
       end
     end
 
     context 'when showing all the examination boards of the TCC two calendar' do
       let(:orientation) { examination_board_tcc_two.orientation }
       let(:academic) { orientation.academic }
-      let!(:academic_activity) do
+      let(:academic_activity) do
         create(:monograph_academic_activity, academic: academic,
                                              calendar: orientation.calendars.first)
       end
@@ -100,36 +44,59 @@ describe 'ExaminationBoard::index', type: :feature, js: true do
       end
 
       it 'shows Monografia for TCC2' do
-        click_link('Monografia')
-        expect(page).to have_content('Monografia')
-
-        within('div#tabContent') do
-          advisor_name = examination_board_tcc_two.orientation.advisor.name_with_scholarity
-
-          expect(page).to have_contents([long_date(examination_board_tcc_two.date),
-                                         examination_board_tcc_two.place,
-                                         examination_board_tcc_two.orientation.academic.name,
-                                         advisor_name])
-        end
-
-        find("a[data-exam-id='#{examination_board_tcc_two.id}']").click
-
-        expect(page).to have_content(examination_board_tcc_two.academic_activity&.title)
-
-        within("div.examination-board-row.exam_#{examination_board_tcc_two.id}") do
-          examination_board_tcc_two.orientation.supervisors.each do |supervisor|
-            expect(page).to have_content(supervisor.name_with_scholarity)
-          end
-
-          examination_board_tcc_two.professors.each do |professor|
-            expect(page).to have_content(professor.name_with_scholarity)
-          end
-
-          if examination_board_tcc_two.academic_activity
-            expect(page).to have_selector("a[href='#{examination_board_tcc_two.academic_activity.pdf.url}']")
-          end
-        end
+        verify_examination_board(examination_board_tcc_two, 'Monografia')
       end
     end
+  end
+end
+
+def verify_examination_board(examination_board, type)
+  click_link_and_verify_content(type, type)
+  within('div#tabContent') do
+    verify_examination_board_details(examination_board)
+  end
+  find("a[data-exam-id='#{examination_board.id}']").click
+  verify_academic_activity(examination_board)
+  verify_supervisors_and_professors(examination_board)
+end
+
+def click_link_and_verify_content(link_text, content)
+  click_link(link_text)
+  expect(page).to have_content(content)
+end
+
+def verify_examination_board_details(examination_board)
+  expect(page).to have_contents([
+                                  long_date(examination_board.date),
+                                  examination_board.place,
+                                  examination_board.orientation.academic.name,
+                                  examination_board.orientation.advisor.name_with_scholarity
+                                ])
+end
+
+def verify_academic_activity(examination_board)
+  academic_activity = examination_board.academic_activity
+  expect(page).to have_content(academic_activity&.title)
+  within("div.examination-board-row.exam_#{examination_board.id}") do
+    expect(page).to have_selector("a[href='#{academic_activity.pdf.url}]") if academic_activity&.pdf
+  end
+end
+
+def verify_supervisors_and_professors(examination_board)
+  within("div.examination-board-row.exam_#{examination_board.id}") do
+    verify_supervisors(examination_board.orientation.supervisors)
+    verify_professors(examination_board.professors)
+  end
+end
+
+def verify_supervisors(supervisors)
+  supervisors.each do |supervisor|
+    expect(page).to have_content(supervisor.name_with_scholarity)
+  end
+end
+
+def verify_professors(professors)
+  professors.each do |professor|
+    expect(page).to have_content(professor.name_with_scholarity)
   end
 end
