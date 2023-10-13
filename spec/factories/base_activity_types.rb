6336899e7448a5a8841b3cf874002c55f81f2f61
Diego Marczal <dmarczal@gmail.com>
2023-10-13 16:48:03 -0300

    Review PR #228 issue #204 - Added missing model test

--- spec/factories/base_activity_types.rb
+++ spec/factories/base_activity_types.rb
@@ -3,7 +3,11 @@ FactoryBot.define do
     sequence(:name) { |n| "base_activity_type#{n}" }
     identifier { :send_document }
 
-    factory :base_activity_info do
+    factory :base_activity_type_send_document do
+      identifier { :send_document }
+    end
+
+    factory :base_activity_type_info do
       identifier { :info }
     end
   end
