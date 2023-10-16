1641b640ef8002468b38dea86a63568ba83f588f
Diego Marczal <dmarczal@gmail.com>
2023-10-16 06:39:20 -0300

    Fixed eslint issues

--- app/javascript/initializers/examination-boards.js
+++ app/javascript/initializers/examination-boards.js
@@ -9,12 +9,12 @@ export default {
         element.addEventListener('click', function(event) {
           const ebId = this.dataset.ebId;
           const el = document.getElementById('eb-' + ebId);
-            if (el.classList.contains('d-none')) {
-              el.classList.remove('d-none');
-            } else {
-              el.classList.add('d-none');
-            }
-            event.preventDefault()
+          if (el.classList.contains('d-none')) {
+            el.classList.remove('d-none');
+          } else {
+            el.classList.add('d-none');
+          }
+          event.preventDefault();
         });
       });
     }
