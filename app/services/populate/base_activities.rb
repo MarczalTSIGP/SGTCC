70d3642c59e445e107ebfa7f55305deca50b3aff
Ligia Falcao <lih.falcao@hotmail.com>
2023-10-16 19:11:08 -0300

    Rubocop

--- app/services/populate/base_activities.rb
+++ app/services/populate/base_activities.rb
@@ -15,75 +15,75 @@ class Populate::BaseActivities
   private
 
   def create_base_activities
-    tcc1 = @tccs['one']
-    tcc2 = @tccs['two']
+    tcc_one = @tccs['one']
+    tcc_two = @tccs['two']
 
     BaseActivity.create!(
       name: 'Assinatura do Termo de Compromisso de Orientação',
-      tcc: tcc1,
+      tcc: tcc_one,
       base_activity_type_id: @base_activity_type_ids.sample,
       identifier: @tcc_one_identifiers.sample,
       judgment: Faker::Boolean.boolean
     )
     BaseActivity.create!(
       name: 'Envio da Proposta',
-      tcc: tcc1,
+      tcc: tcc_one,
       base_activity_type_id: @base_activity_type_ids.sample,
       identifier: @tcc_one_identifiers.sample,
       judgment: Faker::Boolean.boolean
     )
     BaseActivity.create!(
       name: 'Defesa da Proposta',
-      tcc: tcc1,
+      tcc: tcc_one,
       base_activity_type_id: @base_activity_type_ids.sample,
       identifier: @tcc_one_identifiers.sample,
       judgment: Faker::Boolean.boolean
     )
     BaseActivity.create!(
       name: 'Envio da Versão Final da Proposta',
-      tcc: tcc1,
+      tcc: tcc_one,
       base_activity_type_id: @base_activity_type_ids.sample,
       identifier: @tcc_one_identifiers.sample,
       judgment: Faker::Boolean.boolean
     )
     BaseActivity.create!(
       name: 'Envio do Projeto',
-      tcc: tcc1,
+      tcc: tcc_one,
       base_activity_type_id: @base_activity_type_ids.sample,
       identifier: @tcc_one_identifiers.sample,
       judgment: Faker::Boolean.boolean
     )
     BaseActivity.create!(
       name: 'Defesa do Projeto',
-      tcc: tcc1,
+      tcc: tcc_one,
       base_activity_type_id: @base_activity_type_ids.sample,
       identifier: @tcc_one_identifiers.sample,
       judgment: Faker::Boolean.boolean
     )
     BaseActivity.create!(
       name: 'Envio da Versão Final do Projeto',
-      tcc: tcc1,
+      tcc: tcc_one,
       base_activity_type_id: @base_activity_type_ids.sample,
       identifier: @tcc_one_identifiers.sample,
       judgment: Faker::Boolean.boolean
     )
     BaseActivity.create!(
       name: 'Envio da Monografia',
-      tcc: tcc2,
+      tcc: tcc_two,
       base_activity_type_id: @base_activity_type_ids.sample,
       identifier: @identifiers.last,
       judgment: Faker::Boolean.boolean
     )
     BaseActivity.create!(
       name: 'Defesa da Monografia',
-      tcc: tcc2,
+      tcc: tcc_two,
       base_activity_type_id: @base_activity_type_ids.sample,
       identifier: @identifiers.last,
       judgment: Faker::Boolean.boolean
     )
     BaseActivity.create!(
       name: 'Envio da Versão Final da Monografia',
-      tcc: tcc2,
+      tcc: tcc_two,
       base_activity_type_id: @base_activity_type_ids.sample,
       identifier: @identifiers.last,
       judgment: Faker::Boolean.boolean
