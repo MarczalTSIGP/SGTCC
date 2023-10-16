4117a53f2bd63ca3d038f84b1687fcf1a18a9e35
tankesho <takeoga@hotmail.com>
2023-10-15 22:05:59 -0300

    criação dos testes

--- spec/features/professors/meetings/meetings_index_spec.rb
+++ spec/features/professors/meetings/meetings_index_spec.rb
@@ -36,5 +36,48 @@ describe 'Meeting::index', type: :feature, js: true do
         end
       end
     end
+
+    context 'when shows all meetings by date' do
+      it 'shows all meetings by date when created in order' do
+        meetings = [create(:meeting, orientation: orientation, date: 2.days.from_now),
+                    create(:meeting, orientation: orientation, date: 1.days.from_now),
+                    create(:meeting, orientation: orientation, date: Time.now)]
+
+        visit professors_meetings_path
+
+        within('table.table tbody') do
+          meetings.each_with_index do |meeting, index|
+            child = index + 1
+            base_selector = "tr:nth-child(#{child})"
+
+            expect(page).to have_selector("#{base_selector} a[href='#{professors_meeting_path(meeting)}']",
+                                          text: meeting.orientation.academic_with_calendar)
+            expect(page).to have_selector(base_selector,
+                                          text: short_date(meeting.date))
+          end
+        end
+      end
+      it 'shows all meetings by date when created in reverse order' do
+        meeting1 = create(:meeting, orientation: orientation, date: Time.now)
+	meeting2 = create(:meeting, orientation: orientation, date: 1.days.from_now)
+	meeting3 = create(:meeting, orientation: orientation, date: 2.days.from_now)
+
+	meetings = [meeting3, meeting2, meeting1]
+
+        visit professors_meetings_path
+
+        within('table.table tbody') do
+          meetings.each_with_index do |meeting, index|
+          child = index + 1
+          base_selector = "tr:nth-child(#{child})"
+
+          expect(page).to have_selector("#{base_selector} a[href='#{professors_meeting_path(meeting)}']",
+                                        text: meeting.orientation.academic_with_calendar)
+          expect(page).to have_selector(base_selector,
+                                        text: short_date(meeting.date))
+          end
+        end
+      end
+    end
   end
 end
