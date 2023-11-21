de6c7e8393a61233f0b08f580103dba2b612af4e
camilaesouza <camila-emanuele@hotmail.com>
2023-11-20 21:01:25 -0300

    add tests by tcc type

--- spec/features/external_members/examination_boards/examination_boards_index_spec.rb
+++ spec/features/external_members/examination_boards/examination_boards_index_spec.rb
@@ -2,30 +2,39 @@ require 'rails_helper'
 
 describe 'ExaminationBoard::index', type: :feature, js: true do
   let(:external_member) { create(:external_member) }
-  let!(:examination_board) { create(:examination_board_tcc_one) }
+  let!(:examination_board_tcc_one) { create(:examination_board_tcc_one) }
+  let!(:examination_board_tcc_two) { create(:examination_board_tcc_two) }
 
   before do
-    examination_board.external_members << external_member
+    examination_board_tcc_one.external_members << external_member
+    examination_board_tcc_two.external_members << external_member
     login_as(external_member, scope: :external_member)
     visit external_members_examination_boards_path
   end
 
   describe '#index' do
     context 'when shows the examination boards of the tcc one calendar' do
+      before do
+        visit external_members_examination_boards_tcc_one_path
+      end
       it 'shows the examination boards of the tcc one with options' do
-        expect(page).to have_link(examination_board.orientation.academic_with_calendar,
-                                  href: external_members_examination_board_path(examination_board))
-        expect(page).to have_contents([examination_board.orientation.advisor.name_with_scholarity,
-                                       examination_board.place,
-                                       datetime(examination_board.date)])
-
-        # examination_board.professors.each do |professor|
-        #   expect(page).to have_content(professor.name_with_scholarity)
-        # end
-        #
-        # examination_board.external_members.each do |external_member|
-        #   expect(page).to have_content(external_member.name_with_scholarity)
-        # end
+        expect(page).to have_link(examination_board_tcc_one.orientation.academic_with_calendar,
+                                  href: external_members_examination_board_path(examination_board_tcc_one))
+        expect(page).to have_contents([examination_board_tcc_one.orientation.advisor.name_with_scholarity,
+                                       examination_board_tcc_one.place,
+                                       datetime(examination_board_tcc_one.date)])
+      end
+    end
+    context 'when shows the examination boards of the tcc two calendar' do
+      before do
+        visit external_members_examination_boards_tcc_two_path
+      end
+      it 'shows the examination boards of the tcc one with options' do
+        expect(page).to have_link(examination_board_tcc_two.orientation.academic_with_calendar,
+                                  href: external_members_examination_board_path(examination_board_tcc_two))
+        expect(page).to have_contents([examination_board_tcc_two.orientation.advisor.name_with_scholarity,
+                                       examination_board_tcc_two.place,
+                                       datetime(examination_board_tcc_two.date)])
       end
     end
   end
--- spec/features/professors/examination_boards/examination_boards_index_spec.rb
+++ spec/features/professors/examination_boards/examination_boards_index_spec.rb
@@ -2,30 +2,41 @@ require 'rails_helper'
 
 describe 'ExaminationBoard::index', type: :feature, js: true do
   let(:professor) { create(:professor) }
-  let!(:examination_board) { create(:examination_board_tcc_one) }
+  let!(:examination_board_tcc_one) { create(:examination_board_tcc_one) }
+  let!(:examination_board_tcc_two) { create(:examination_board_tcc_two) }
 
   before do
-    examination_board.professors << professor
+    examination_board_tcc_one.professors << professor
+    examination_board_tcc_two.professors << professor
     login_as(professor, scope: :professor)
-    visit professors_examination_boards_path
   end
 
   describe '#index' do
     context 'when shows the examination boards of the tcc one calendar' do
-      it 'shows the examination boards of the tcc one with options' do
-        expect(page).to have_link(examination_board.orientation.academic_with_calendar,
-                                  href: professors_examination_board_path(examination_board))
-        expect(page).to have_contents([examination_board.orientation.advisor.name_with_scholarity,
-                                       examination_board.place,
-                                       datetime(examination_board.date)])
+      before do
+        visit professors_examination_boards_tcc_one_path
+      end
 
-        # examination_board.professors.each do |professor|
-        #   expect(page).to have_content(professor.name_with_scholarity)
-        # end
-        #
-        # examination_board.external_members.each do |external_member|
-        #   expect(page).to have_content(external_member.name_with_scholarity)
-        # end
+      it 'shows the examination boards of the tcc one with options' do
+        expect(page).to have_link(examination_board_tcc_one.orientation.academic_with_calendar,
+                                  href: professors_examination_board_path(examination_board_tcc_one))
+        expect(page).to have_contents([examination_board_tcc_one.orientation.advisor.name_with_scholarity,
+                                       examination_board_tcc_one.place,
+                                       datetime(examination_board_tcc_one.date)])
+      end
+    end
+
+    context 'when shows the examination boards of the tcc two calendar' do
+      before do
+        visit professors_examination_boards_tcc_two_path
+      end
+
+      it 'shows the examination boards of the tcc two with options' do
+        expect(page).to have_link(examination_board_tcc_two.orientation.academic_with_calendar,
+                                  href: professors_examination_board_path(examination_board_tcc_two))
+        expect(page).to have_contents([examination_board_tcc_two.orientation.advisor.name_with_scholarity,
+                                       examination_board_tcc_two.place,
+                                       datetime(examination_board_tcc_two.date)])
       end
     end
   end
