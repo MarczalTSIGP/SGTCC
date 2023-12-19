d9e36cc0c9f3e4542db3d31fa705521b3a51d94d
Prof. Diego Marczal <37582258+MarczalTSIGP@users.noreply.github.com>
2023-12-19 09:21:53 -0300

    Fixed orientation current_calendar to return correctly calendar (#294)
    * Fixed orientation current_calendar to return correctly calendar
    
    * Fixed DEPRECATION WARNING: #extension_whitelist is deprecated, use #extension_allowlist instead.
    
    ---------
    
    Co-authored-by: Diego Marczal <dmarczal@gmail.com>

--- app/uploaders/zip_uploader.rb
+++ app/uploaders/zip_uploader.rb
@@ -27,7 +27,7 @@ class ZipUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  def extension_whitelist
+  def extension_allowlist
     %w[zip]
   end
 
