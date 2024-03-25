1e4eec33058bf20cbd46347eefcbf57f7d3eaf20
Diego Marczal <dmarczal@gmail.com>
2024-03-25 08:47:32 -0300

    Updated Moodle authentication

--- lib/ldap/ldap_authentication.rb
+++ lib/ldap/ldap_authentication.rb
@@ -45,7 +45,7 @@ module SGTCC
       form.field_with(id: 'password').value = pwd
       resp = form.submit
 
-      resp.title.eql?('Painel | Moodle UTFPR')
+      resp.link_with(href: 'https://moodle.utfpr.edu.br/my/courses.php').present?
     end
 
     private_class_method :base_authenticate
