d694c8162601d6b4863aa8469640d5cb4c3651b2
Guilherme Staciaki <gstaciaki99@gmail.com>
2025-05-28 20:57:12 -0300

    chore: removed unnecessary if statement on migration

--- db/migrate/20191004191641_add_document_available_until_to_examination_boards.rb
+++ db/migrate/20191004191641_add_document_available_until_to_examination_boards.rb
@@ -1,8 +1,6 @@
 class AddDocumentAvailableUntilToExaminationBoards < ActiveRecord::Migration[5.2]
   def up
-    unless column_exists?(:examination_boards, :document_available_until)
-      add_column :examination_boards, :document_available_until, :timestamp
-    end
+    add_column :examination_boards, :document_available_until, :timestamp
 
     execute <<-SQL.squish
       UPDATE examination_boards
@@ -11,8 +9,6 @@ class AddDocumentAvailableUntilToExaminationBoards < ActiveRecord::Migration[5.2
   end
 
   def down
-    return unless column_exists?(:examination_boards, :document_available_until)
-
     remove_column :examination_boards, :document_available_until
   end
 end
