3fdde884127052e3c1f8e42f0219adc91ba47d6e
Guilherme Staciaki <gstaciaki99@gmail.com>
2025-05-28 22:52:24 -0300

    fix: dropdown menu position

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,3 +6,4 @@ pin '@hotwired/stimulus', to: 'stimulus.min.js'
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js'
 pin_all_from 'app/javascript/controllers', under: 'controllers'
 pin 'bootstrap', to: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js'
+pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
