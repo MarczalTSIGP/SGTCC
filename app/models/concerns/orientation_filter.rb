8628708894d1b6efc2b3d80d620ef94527f5ce5e
Diego Marczal <dmarczal@gmail.com>
2025-08-06 21:19:51 -0300

    Fix pagination bug (#354)

--- app/models/concerns/orientation_filter.rb
+++ app/models/concerns/orientation_filter.rb
@@ -30,9 +30,5 @@ module OrientationFilter
     def self.by_current_tcc_two(page, term, status = nil)
       by_tcc(current_tcc_two(status), page, term).recent
     end
-
-    def self.migratable(page, term)
-      by_tcc(to_migrate, page, term).recent
-    end
   end
 end
