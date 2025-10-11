65e9fc21ce1ac82311adc41957f06f67ddbb271b
Guilherme Staciaki <gstaciaki99@gmail.com>
2025-10-11 01:44:33 -0300

    test(helper): better method to get buttons

--- spec/support/helpers/link.rb
+++ spec/support/helpers/link.rb
@@ -1,8 +1,7 @@
 module Helpers
   module Link
-    def click_on_destroy_link(url)
-      destroy_link = "a[href='#{url}'][data-method='delete']"
-      find(destroy_link).click
+    def click_on_destroy_link(path)
+      find("a[href='#{path}'][data-method='delete'], a[href='#{path}'][data-turbo-method='delete']").click
     end
 
     def link(link)
