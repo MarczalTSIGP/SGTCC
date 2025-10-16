8dc59e86b140cdccfa17fbcc79b8f2253f759852
Diego Marczal <dmarczal@gmail.com>
2025-10-16 09:23:04 -0300

    fix: tooltip

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,20 +1,9 @@
-import { Application } from "@hotwired/stimulus";
+import { Application } from '@hotwired/stimulus';
 
 const application = Application.start();
 
 // Configure Stimulus development experience
 application.debug = false;
-window.Stimulus   = application;
+window.Stimulus = application;
 
 export { application };
-
-document.addEventListener("turbo:load", () => {
-  const tooltipTriggerList = [].slice.call(
-    document.querySelectorAll("[data-bs-toggle=\"tooltip\"]")
-  );
-  tooltipTriggerList.forEach((tooltipTriggerEl) => {
-    new bootstrap.Tooltip(tooltipTriggerEl, {
-      delay: { show: 0, hide: 0 },
-    });
-  });
-});
