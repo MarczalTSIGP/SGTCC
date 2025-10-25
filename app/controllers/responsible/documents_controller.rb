b9523ac53376d122ad52005db9019b55b81a4572
Guilherme Staciaki <gstaciaki99@gmail.com>
2025-10-25 00:02:23 -0300

    refactor(responsible): document review view with correct behavior

--- app/controllers/responsible/documents_controller.rb
+++ app/controllers/responsible/documents_controller.rb
@@ -7,11 +7,30 @@ class Responsible::DocumentsController < Responsible::BaseController
 
   def judgment
     if @document.professor_signed?(current_professor)
-      render json: { status: false,
-                     message: I18n.t('json.messages.documents.errors.update') }
+      flash.now[:sweet_error] = I18n.t('json.messages.documents.errors.update')
+    elsif @document.save_judgment(current_professor, judgment_params)
+      flash.now[:sweet_success] = I18n.t('json.messages.documents.success.update')
+      @document.reload
     else
-      render json: { status: @document.save_judgment(current_professor, params),
-                     message: I18n.t('json.messages.documents.success.update') }
+      flash.now[:sweet_error] = I18n.t('json.messages.empty_fields')
+    end
+
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: [
+          turbo_stream.replace(
+            "document_review_#{@document.id}",
+            partial: 'shared/documents/document_review',
+            locals: { 
+              document: @document,
+              has_permission: current_professor.role?('responsible'),
+              can_edit: true
+            }
+          ),
+          turbo_stream.append('app', partial: 'shared/sweet_alert')
+        ]
+      end
+      format.html { redirect_back(fallback_location: responsible_root_path) }
     end
   end
 
@@ -19,5 +38,9 @@ class Responsible::DocumentsController < Responsible::BaseController
 
   def set_document
     @document = Document.find(params[:id])
+  end
+
+  def judgment_params
+    params.permit(:accept, :justification)
   end
 end
