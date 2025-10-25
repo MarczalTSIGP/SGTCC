ff7cb3075139365da09038d0a88054972d46bf59
Guilherme Staciaki <gstaciaki99@gmail.com>
2025-10-25 00:34:51 -0300

    feat(responsible): replaces vue component status label to a new one

---
+++ app/components/documents/status_badge_component.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+class Documents::StatusBadgeComponent < ViewComponent::Base
+  def initialize(request:)
+    @request = request || {}
+  end
+
+  def badge_class
+    "badge badge-#{badge_status}"
+  end
+
+  def status_label
+    return 'Deferido' if accepted?
+    return 'Indeferido' if refused?
+
+    'Em análise'
+  end
+
+  private
+
+  def badge_status
+    return 'success' if accepted?
+    return 'danger' if refused?
+
+    'primary'
+  end
+
+  def has_judgment?
+    @request.dig('judgment').is_a?(Hash)
+  end
+
+  def accept_value
+    @request.dig('judgment', 'responsible', 'accept')
+  end
+
+  def accepted?
+    has_judgment? && accept_value == 'true'
+  end
+
+  def refused?
+    has_judgment? && accept_value == 'false'
+  end
+end
+
