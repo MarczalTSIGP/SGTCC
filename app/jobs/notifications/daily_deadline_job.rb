eaf146a3afc305ee9ceda343c9278ec37be945e0
Raul Ferreira da Rocha <raulferreiradarocha1@hotmail.com>
2025-11-06 14:47:20 -0300

    Add create job to create new notifications on workers container

--- app/jobs/notifications/daily_deadline_job.rb
+++ app/jobs/notifications/daily_deadline_job.rb
@@ -2,6 +2,6 @@ class Notifications::DailyDeadlineJob < ApplicationJob
   queue_as :notifications
 
   def perform
-    Notifications::Hooks.daily_deadline_notifications
+    Notifications::Hooks::Activity.daily_deadline_notifications
   end
 end
--- app/services/notifications/hook_helpers.rb
+++ app/services/notifications/hook_helpers.rb
@@ -1,12 +1,12 @@
 module Notifications
   module HookHelpers
     def schedule_notification(notification_type:, recipient:, data:, event_key:)
-      ::Notifications::SchedulerService.new(
+      Notifications::CreateJob.perform_later(
         notification_type: notification_type,
         recipient: recipient,
         data: data,
         event_key: event_key
-      ).schedule!
+      )
     end
 
     def event_key(*parts)
