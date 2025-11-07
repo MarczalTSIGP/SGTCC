504b542eebf254074f9b596c73dd2b4f6dd15ba8
Raul Ferreira da Rocha <raulferreiradarocha1@hotmail.com>
2025-11-07 10:26:53 -0300

    Add jobs tests. Chg enviroment test to can test the jobs

--- spec/jobs/notifications/dispatch_job_spec.rb
+++ spec/jobs/notifications/dispatch_job_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
-RSpec.describe Notifications::DispatchJob do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Notifications::DispatchJob, type: :job do
+  let!(:notification) { create(:notification) }
+
+  let(:dispatcher) { instance_double(Notifications::Dispatcher, call: true) }
+
+  before do
+    allow(Notifications::Dispatcher).to receive(:new).and_return(dispatcher)
+  end
+
+  context 'quando a notificação existe' do
+    it 'chama o Dispatcher com a notificação correta' do
+      described_class.perform_now(notification.id)
+      
+      expect(Notifications::Dispatcher).to have_received(:new).with(notification).once
+      expect(dispatcher).to have_received(:call).once
+    end
+
+   it 're-levanta o erro se o Dispatcher falhar (para retries)' do
+    allow(dispatcher).to receive(:call).and_raise(StandardError, 'Erro de SMTP')
+
+    expect { described_class.perform_now(notification.id) }
+      .to raise_error(StandardError, 'Erro de SMTP')
+    end
+  end
+
+  context 'quando a notificação não existe' do
+    it 'não faz nada e loga um aviso' do
+      
+      described_class.perform_now(9999)
+      
+      expect(Notifications::Dispatcher).not_to have_received(:new)
+    end
+  end
 end
--- spec/jobs/notifications/scheduler_poller_job_spec.rb
+++ spec/jobs/notifications/scheduler_poller_job_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
-RSpec.describe Notifications::SchedulerPollerJob do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Notifications::SchedulerPollerJob, type: :job do
+  include ActiveSupport::Testing::TimeHelpers
+
+  let(:processor_instance_spy) { instance_double(Notifications::ProcessorService, process: true) }
+
+  before do
+    allow(Notifications::ProcessorService).to receive(:new).with(anything).and_return(processor_instance_spy)
+  end
+  
+  it 'chama o ProcessorService para cada notificação pendente' do
+    travel_to Time.current do
+      notification_pending = create(:notification, status: 'pending')
+      notification_scheduled_past = create(:notification, status: 'scheduled', scheduled_at: 1.hour.ago)
+      
+      create(:notification, status: 'scheduled', scheduled_at: 1.hour.from_now)
+      create(:notification, status: 'sent')
+
+      described_class.perform_now
+
+      expect(Notifications::ProcessorService).to have_received(:new).with(notification_pending).once
+      expect(Notifications::ProcessorService).to have_received(:new).with(notification_scheduled_past).once
+      expect(Notifications::ProcessorService).to have_received(:new).exactly(2).times
+
+      expect(processor_instance_spy).to have_received(:process).with(no_args).exactly(2).times
+    end
+  end
+
+  it 'não chama o ProcessorService se não houver notificações pendentes' do
+    travel_to Time.current do
+      create(:notification, status: 'sent')
+      create(:notification, status: 'scheduled', scheduled_at: 1.hour.from_now)
+
+      described_class.perform_now
+
+      expect(Notifications::ProcessorService).not_to have_received(:new)
+      expect(processor_instance_spy).not_to have_received(:process)
+    end
+  end
 end
