4bae19f904f9cf65831d64d74bc87b27efe53255
Diego Marczal <marczal@utfpr.edu.br>
2026-03-03 15:48:55 -0300

    Remove unecessary files and fixed tests

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -37,6 +37,7 @@ Rails.application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { host: ENV.fetch("APP_HOST", "localhost") }
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
@@ -44,6 +45,8 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
+
+Rails.application.routes.default_url_options[:host] = ENV.fetch("APP_HOST", "localhost")
 
 CarrierWave.configure do |config|
   config.storage = :file
