a0f36da21652f15a03bcbfe983835bb80e0eb2cb
Diego Marczal <marczal@utfpr.edu.br>
2026-06-12 18:39:31 -0300

    improve examination board note range selector

--- app/inputs/range_tabler_input.rb
+++ app/inputs/range_tabler_input.rb
@@ -3,12 +3,11 @@ class RangeTablerInput < SimpleForm::Inputs::Base
     merged_input_options = merge_wrapper_options(input_html_options, wrapper_options)
     value = object.public_send(attribute_name) || merged_input_options[:value] || 0
 
-    @builder.range_field(attribute_name,
-                         build_input_options(merged_input_options, value)) + error_html
+    slider_html(merged_input_options, value) + error_html
   end
 
   def label(_wrapper_options)
-    value = object.public_send(attribute_name) || input_html_options[:value] || 0
+    value = object.public_send(attribute_name)
 
     template.tag.label(class: 'form-label range-label') do
       template.concat summary_card(value)
@@ -16,6 +15,16 @@ class RangeTablerInput < SimpleForm::Inputs::Base
   end
 
   private
+
+  def slider_html(merged_input_options, value)
+    template.tag.div(class: 'range-slider') do
+      template.concat selected_note_output(value)
+      template.concat @builder.range_field(
+        attribute_name,
+        build_input_options(merged_input_options, value)
+      )
+    end
+  end
 
   def build_input_options(merged_input_options, value)
     merged_input_options.deep_merge(
@@ -31,29 +40,26 @@ class RangeTablerInput < SimpleForm::Inputs::Base
   end
 
   def input_css_class(custom_classes)
-    [custom_classes, 'form-control-range', input_error_class].compact.join(' ')
+    [custom_classes, 'form-control-range pl-0 pr-0', input_error_class].compact.join(' ')
   end
 
   def summary_card(value)
     template.tag.div(class: 'range-summary-card') do
       template.concat current_note_card(value)
-      template.concat selected_note_card(value)
     end
   end
 
   def current_note_card(value)
     template.tag.div(class: 'range-summary-item range-summary-item-current') do
-      title = I18n.t('activerecord.attributes.examination_board_note.actual_note')
+      title = note_title(value)
       template.concat summary_title(title)
-      template.concat summary_value(value)
+      template.concat summary_value(value) unless value.nil?
     end
   end
 
-  def selected_note_card(value)
-    template.tag.div(class: 'range-summary-item range-summary-item-selected') do
-      template.concat summary_title(I18n.t('views.labels.selected.f'))
-      template.concat selected_note_span(value)
-    end
+  def note_title(value)
+    key = value.nil? ? 'unassigned_note' : 'actual_note'
+    I18n.t("activerecord.attributes.examination_board_note.#{key}")
   end
 
   def summary_title(text)
@@ -64,9 +70,10 @@ class RangeTablerInput < SimpleForm::Inputs::Base
     template.content_tag(:span, value, class: 'range-summary-value')
   end
 
-  def selected_note_span(value)
-    template.tag.span(value, class: 'range-summary-value',
-                             data: { range_tabler_selected_note: true })
+  def selected_note_output(value)
+    template.tag.output(value,
+                        class: 'range-selected-value',
+                        data: { range_tabler_selected_note: true })
   end
 
   def error_html
--- app/javascript/controllers/forms/range_tabler_controller.js
+++ app/javascript/controllers/forms/range_tabler_controller.js
@@ -2,17 +2,33 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   connect() {
+    this.resizeObserver = new ResizeObserver(() => this.update());
+    this.resizeObserver.observe(this.element);
     this.update();
   }
 
+  disconnect() {
+    this.resizeObserver?.disconnect();
+  }
+
   update() {
-    const value = this.element.value || 0;
-    const wrapper = this.element.closest(".input, .form-group, .mb-3") || this.element.parentElement;
+    const value = Number(this.element.value || 0);
+    const min = Number(this.element.min || 0);
+    const max = Number(this.element.max || 100);
+    const range = max - min;
+    const rawRatio = range > 0 ? (value - min) / range : 0;
+    const ratio = Math.min(1, Math.max(0, rawRatio));
+    const thumbWidth = 14;
+    const left = this.element.offsetLeft
+      + (thumbWidth / 2)
+      + (ratio * (this.element.clientWidth - thumbWidth));
+    const selectedNote = this.element.parentElement.querySelector(
+      "[data-range-tabler-selected-note]"
+    );
 
-    if (!wrapper) return;
-
-    const selectedNote = wrapper.querySelector("[data-range-tabler-selected-note]");
-
-    if (selectedNote) selectedNote.textContent = value;
+    if (selectedNote) {
+      selectedNote.textContent = value;
+      selectedNote.style.left = `${left}px`;
+    }
   }
 }
